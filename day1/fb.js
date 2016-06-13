439738cd2c2e987c6d01259de7210992f08280d8
aschugart <annieschugart@Annies-MacBook-Pro-2.local>
2016-06-13 11:03:58 -0400

    jafdas

---
+++ day1/fb.js
@@ -0,0 +1,2 @@
+    "id": "575ebd221fc741fd28d1b582",
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFzY2h1Z2FydEBjb2xsZWdlLmhhcnZhcmQuZHUifQ.kbP5e0_cJFpyCOwQRetoK8aW1BR6wj-BsLC-Sz5Exd0"
