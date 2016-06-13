930ae6e6c63791f0a5dab144c65fb9cd8cf33d43
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-13 14:00:03 -0500

    Refactored

--- day2/wikipedia/wikipedia.js
+++ day2/wikipedia/wikipedia.js
@@ -1,41 +1,98 @@
+//var _ = require("../lib/underscore")
 var fs = require('fs');
 var readline = require('readline');
 var stream = require('stream');
 var instream = fs.createReadStream('asd2');
 var outstream = new stream;
-var rl = readline.createInterface(instream, outstream);
-var topResults = [];
-var lastValueOfTop10=0;
+//var rl = readline.createInterface(instream, outstream);
 
-rl.on('line', function(line) {
-  /*var obj=parseLine(line)
-  if (topResults.length<=10){
-    topResults.push(obj)
-  }else{
-    if (obj.count>lastValueOfTop10){
+function getTopArticles(){
+  var topResults = [];
+  var lastValueOfTop10=0;
+  var rl = readline.createInterface(instream, outstream);
+  rl.on('line', function(line) {
+
+  //  Get top articles
+    var obj=parseLine(line)
+    if (topResults.length<=10){
       topResults.push(obj)
-      topResults.sort(sort);
-      topResults.splice(-1,1);
-      lastValueOfTop10=topResults[topResults.length-1].count;
+    }else{
+      if (obj.count>lastValueOfTop10){
+        topResults.push(obj)
+        topResults.sort(sort);
+        topResults.splice(-1,1);
+        lastValueOfTop10=topResults[topResults.length-1].count;
+      }
     }
-  }*/
-});
+  });
 
-rl.on('close', function() {
-  // do something on finish here
-  console.log(topResults)
-});
+  rl.on('close', function() {
+    console.log(topResults)
+  });
 
-
-var parseLine = function (line){
-  var lineArray = line.split(" ");
-  return {
-    domain:lineArray[0],
-    name:lineArray[1],
-    count:lineArray[2]
+  var parseLine = function (line){
+    var lineArray = line.split(" ");
+    return {
+      domain:lineArray[0],
+      name:lineArray[1],
+      count:lineArray[2]
+    }
   }
+  var sort = function(a, b, sortBy) {
+    return parseInt(b.count) - parseInt(a.count);
+  };
 }
-var sort = function(a, b) {
+
+getTopArticles()
+
+
+
+
+
+
+function getTopLanguages(){
+  var topResults = [];
+  var lastValueOfTop10=0;
+  var rl = readline.createInterface(instream, outstream);
+  rl.on('line', function(line) {
+
+  //  Get top articles
+    var obj=parseLine(line)
+    if (topResults.length<=10){
+      topResults.push(obj)
+    }else{
+      if (obj.count>lastValueOfTop10){
+        topResults.push(obj)
+        topResults.sort(sort);
+        topResults.splice(-1,1);
+        lastValueOfTop10=topResults[topResults.length-1].count;
+      }
+    }
+  });
+
+  rl.on('close', function() {
+    console.log(topResults)
+  });
+
+  var parseLine = function (line){
+    var lineArray = line.split(" ");
+    return {
+      domain:lineArray[0],
+      name:lineArray[1],
+      count:lineArray[2]
+    }
+  }
+  var sort = function(a, b, sortBy) {
+    return parseInt(b.count) - parseInt(a.count);
+  };
+}
+
+
+
+
+
+
+var sortByLang = function(a, b, sortBy) {
   return parseInt(b.count) - parseInt(a.count);
 };
 
