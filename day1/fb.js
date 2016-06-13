a455ecbc803c15b7f627ac5f454c21c4c47950b4
aschugart <annieschugart@Annies-MacBook-Pro-2.local>
2016-06-13 11:33:59 -0400

    want to pull

--- day1/fb.js
+++ day1/fb.js
@@ -1,2 +1,22 @@
-    "id": "575ebd221fc741fd28d1b582",
-    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFzY2h1Z2FydEBjb2xsZWdlLmhhcnZhcmQuZHUifQ.kbP5e0_cJFpyCOwQRetoK8aW1BR6wj-BsLC-Sz5Exd0"
+    // "id": "575ebd221fc741fd28d1b582",
+    // "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFzY2h1Z2FydEBjb2xsZWdlLmhhcnZhcmQuZHUifQ.kbP5e0_cJFpyCOwQRetoK8aW1BR6wj-BsLC-Sz5Exd0"
+
+var apiURL = "https://fb.horizonsbootcamp.com/api/1.0"
+
+$.ajax(apiUrl + "/users/register", {
+      method: "POST",
+      data: {
+        email: horello.apiKey,
+        password: horello.apiToken,
+        fname: name,
+        lname: desc,
+        birthMonth: this.getId(),
+        birthDay: ,
+        birthYear:
+      },
+      success: function (data) {
+      },
+      error: function (err) {
+        console.error("Error");
+      }
+    }
