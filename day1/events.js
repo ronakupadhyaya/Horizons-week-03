e4e18ca75502a6035b702a42c36b018c717a660d
bw-12 <brendan_woo@brown.edu>
2016-06-13 18:46:50 -0400

    :D

--- day1/events.js
+++ day1/events.js
@@ -2,6 +2,8 @@
 
 window.fb = window.fb || {};
 
+var myToken = null;
+
 fb.mountStatic = function() {
 
 	$('#register').click(function(e) {
@@ -37,7 +39,8 @@ fb.mountStatic = function() {
 			success: function (e2) {
 				console.log("logged that shit");
 				$('#bigBooty').empty();
-				getPosts(e2);
+				myToken = e2.response.token;
+				getPosts();
 			},
 			error: function (err) {
 				console.error("fucked up loggs");
@@ -47,25 +50,70 @@ fb.mountStatic = function() {
 
 }
 
-var getPosts = function(obj) {
+var mountComment = function() {
+$('.commBut').click(function(e){
+		e.preventDefault();
+		$.ajax(fb.apiUrl + '/posts/comments/' + this.attr(id), {
+			method: "POST",
+			data: {
+				token: myToken,
+				content: $('#post').val()
+			},
+			success: function (e2) {
+				console.log("postNew stuff");
+				var myPost = $('<div>' + $('#post').val() + '</div>')
+				$('#bigBooty').empty();
+				getPosts();
+			},
+			error: function (err) {
+				console.error("why cant i post????");
+			}
+		})
+
+	});
+}
+
+var mountPost = function() {
+	$('#postBut').click(function(e){
+		e.preventDefault();
+		$.ajax(fb.apiUrl + '/posts', {
+			method: "POST",
+			data: {
+				token: myToken,
+				content: $('#post').val()
+			},
+			success: function (e2) {
+				console.log("postNew stuff");
+				var myPost = $('<div>' + $('#post').val() + '</div>')
+				$('#bigBooty').empty();
+				getPosts();
+			},
+			error: function (err) {
+				console.error("why cant i post????");
+			}
+		})
+
+	});
+}
+
+var getPosts = function() {
 	$.ajax(fb.apiUrl + '/posts', {
 		method:'GET',
 		data: {
-			token: obj.response.token
+			token: myToken
 		},
 		success: function(obj) {
-			console.log('hell yeaa got back array');
 			for(var i = 0; i<obj.response.length; i++) {
-				console.log(obj.response[i]);
-				var post = $('<div>'+obj.response[i].content+'</div>' + '<div>'+obj.response[i].poster.name+'</div>');
+				var post = $('<div class="posts">'+obj.response[i].content+'</div>' + '<div>'+obj.response[i].poster.name+'<button type="submit" id='+obj.response[i]._id+' class="likebut">Like</button><button type="submit" id='+obj.response[i]._id+' class="commBut">Comments</button></div>');
 				$('#bigBooty').append(post);
 			}
 			var wrapper = $('<div></div>');
 			var message = $('<input type="text" class="form-control" id="post">');
-			var postBut = $('<button type="submit" id="post" class="btn btn-default">Post</button>');
+			var postBut = $('<button type="submit" id="postBut" class="btn btn-default">Post</button>');
 			wrapper.append(message);
 			wrapper.append(postBut);
 			$('#bigBooty').append(wrapper);
+			mount();
 		},
 		error: function(err2) {
 			console.error('noooo didnt get array');
