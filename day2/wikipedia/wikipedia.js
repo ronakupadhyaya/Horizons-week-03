1bc583521abda1bbce2dc333e7ed7ab7ddbae14a
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-13 13:03:35 -0500

    organized code a bit

--- day2/wikipedia/wikipedia.js
+++ day2/wikipedia/wikipedia.js
@@ -1,21 +1,14 @@
 var fs = require('fs');
 var readline = require('readline');
 var stream = require('stream');
-
 var instream = fs.createReadStream('asd2');
 var outstream = new stream;
 var rl = readline.createInterface(instream, outstream);
-
 var topResults = [];
 var lastValueOfTop10=0;
 
 rl.on('line', function(line) {
-  var lineArray = line.split(" ");
-  var obj= {
-    name:lineArray[1],
-    count:lineArray[2]
-  }
-
+  /*var obj=parseLine(line)
   if (topResults.length<=10){
     topResults.push(obj)
   }else{
@@ -25,19 +18,28 @@ rl.on('line', function(line) {
       topResults.splice(-1,1);
       lastValueOfTop10=topResults[topResults.length-1].count;
     }
-  }
-  //domain_code
-  //page_title
-  //count_views
-  //total_response_size
+  }*/
 });
 
 rl.on('close', function() {
   // do something on finish here
-    console.log(topResults)
+  console.log(topResults)
 });
 
 
+var parseLine = function (line){
+  var lineArray = line.split(" ");
+  return {
+    domain:lineArray[0],
+    name:lineArray[1],
+    count:lineArray[2]
+  }
+}
 var sort = function(a, b) {
-    return parseInt(b.count) - parseInt(a.count);
+  return parseInt(b.count) - parseInt(a.count);
 };
+
+//domain_code
+//page_title
+//count_views
+//total_response_size
