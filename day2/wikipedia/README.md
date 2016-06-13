6af7428e51f36928fc1bc69ae7c05858bc4f4bc1
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-13 18:19:35 -0500

    Phase 1 and 2 done

--- day2/wikipedia/README.md
+++ day2/wikipedia/README.md
@@ -8,12 +8,12 @@ The goal of this exercise is to learn how to deal with large real-world datasets
 
 1. Find the top-10 most popular Wikipedia languages
 1. Find the top-10 most popular Wikipedia pages
-1. For the top 10 most popular languages, find the top-10
+1. For the top 3 most popular languages, find the top-10
    most popular pages
 
 ## Phase 2
 
-1. Compare day-over-day traffic to detect top 10 pages and languages that have the largest absolute gain and loss in traffic.
+1. Detect the top 10 pages from day 1. Calculate their gains and losses in traffic by comparing them to day 2.
 
 ## Phase 3
 
@@ -21,51 +21,7 @@ The goal of this exercise is to learn how to deal with large real-world datasets
 
 ## Data files
 
-TODO describe format
+[File Information](https://wikitech.wikimedia.org/wiki/Analytics/Data/Pagecounts-raw)
 
- - [1 hour of Wikipedia traffic from June 6th](https://dumps.wikimedia.org/other/pagecounts-raw/2016/2016-06/pagecounts-20160606-170000.gz)
- - [1 hour of Wikipedia traffic from June 7th](https://dumps.wikimedia.org/other/pagecounts-raw/2016/2016-06/pagecounts-20160607-170000.gz)
-
-
- zu.d iselesele 1 20044
-zu.d isiFulentshi 1 18577
-zu.d isibane 1 18546
-zu.d isibani 1 18811
-zu.d isibindi 1 18790
-zu.d isicabha 1 18660
-zu.d isidlidli 1 18445
-zu.d isidwaba 1 16505
-zu.d isifinyezo 1 17980
-zu.d isifuba 1 18031
-zu.d isigaba 1 18671
-zu.d isigidi 1 17881
-zu.d isigqila 1 17904
-zu.d isigqizo 1 17919
-zu.d isigqoko 1 19946
-zu.d isiguqulo 1 19042
-zu.d isihlabathi 1 19878
-zu.d isihlahla 1 19153
-zu.d isihlakala 1 18136
-zu.d isihlalo 1 21508
-zu.d isihlathi 1 17491
-zu.d isihlelo 1 18181
-zu.d isiketi 1 17871
-zu.d isikhathi 1 18954
-zu.d isikhombisa 1 18591
-zu.d isikhova 1 18256
-zu.d isikhumba 1 18830
-zu.d isikova 1 17987
-zu.d isilevu 1 18769
-zu.d isiliva 1 18420
-zu.d isilokazane 1 18215
-zu.d isilwanyazane 1 18508
-zu.d isimba 1 17768
-zu.d isimema 1 18077
-zu.d isimemo 1 18139
-zu.d isinambuzane 1 18475
-zu.d isineke 1 18877
-zu.d isingane 1 18415
-zu.d isingani 1 18685
-zu.d isintu 1 18456
-zu.d isipikili 1 17932
-zu.d isiqiwu 1 17906
+ - [File #1: 1 hour of Wikipedia traffic from June 6th](https://dumps.wikimedia.org/other/pagecounts-raw/2016/2016-06/pagecounts-20160606-170000.gz)
+ - [File #2: 1 hour of Wikipedia traffic from June 7th](https://dumps.wikimedia.org/other/pagecounts-raw/2016/2016-06/pagecounts-20160607-170000.gz)
--- day2/wikipedia/wikipedia.js
+++ day2/wikipedia/wikipedia.js
@@ -1,15 +1,27 @@
+require('events').EventEmitter.prototype._maxListeners = 100;
 //var _ = require("../lib/underscore")
 var fs = require('fs');
 var readline = require('readline');
 var stream = require('stream');
-var instream = fs.createReadStream('asd2');
-var outstream = new stream;
 
-function getTopArticles(lang){
+// Shared Functions
+var parseLine = function (line){
+  var lineArray = line.split(" ");
+  return {
+    domain:lineArray[0].split(".")[0],
+    name:lineArray[1] || " ",
+    count:parseInt(lineArray[2])
+  }
+}
 
+// PHASE 1
+
+function getTopArticles(callback, lang){
+  var instream = fs.createReadStream('asd2');
+  var outstream = new stream;
+  var rl = readline.createInterface(instream, outstream);
   var topResults = [];
   var lastValueOfTop10=0;
-  var rl = readline.createInterface(instream, outstream);
   rl.on('line', function(line) {
     var obj=parseLine(line)
     if(!lang ||( lang && lang === obj.domain) ){
@@ -27,40 +39,22 @@ function getTopArticles(lang){
       }
     }
   });
-
-  rl.on('close', function() {
-    console.log(topResults)//logs array of article objects
-  });
-
-  var parseLine = function (line){
-    var lineArray = line.split(" ");
-    return {
-      domain:lineArray[0].split(".")[0],
-      name:lineArray[1] || " ",
-      count:lineArray[2]
-    }
-  }
+  rl.on('close', function() { callback(topResults) });
 }
 
 
-
-
-
-//getTopLanguages()
-
-
-function getTopLanguages(){
+function getTopLanguages(callback){
+  var instream = fs.createReadStream('asd2');
+  var outstream = new stream;
+  var rl = readline.createInterface(instream, outstream);
   var topResults = {};
   var lastValueOfTop10=0;
   var rl = readline.createInterface(instream, outstream);
   rl.on('line', function(line) {
     line = line.split(" ");
     var lang =line[0].split(".")[0]
-    //  console.log(lang)
     topResults[lang] = topResults[lang] ?  (topResults[lang] + 1)  : 1;
-
   });
-
   rl.on('close', function() {
     var array=[]
     for(var property in topResults){
@@ -72,27 +66,65 @@ function getTopLanguages(){
     array.sort(function(a, b) {
       return parseInt(b.count) - parseInt(a.count);
     });
-    //TODO Remove commons and other things that arent langs.
-     array= array.slice(0, 10)
-     console.log(array);
+    array=array.slice(0, 10)
+    callback(array);
   });
 }
 
 /*
-function topTenPerLang(){
-  for (int i = 0; i< array.length; i++){
-    connsole.logarray[i].lang
-  }
-}*/
-
-//getTopLanguages();
-//getTopArticles('es')
-getTopArticles()
-//getTopArticles('de')
-//getTopArticles('en')
+// 1. Find the top-10 most popular Wikipedia languages
+getTopLanguages(function(lang){
+console.log(lang)
+} )
+*/
 
 
-//domain_code
-//page_title
-//count_views
-//total_response_size
+/*
+// 2. Find the top-10 most popular Wikipedia pages
+getTopArticles(function(articles){
+console.log(articles)
+} )
+*/
+
+/*
+// For the top 3 most popular languages, find the top-10 most popular pages
+getTopLanguages(function(lang){
+lang= lang.slice(0, 3)
+for (var i=0; i<lang.length; i++){
+getTopArticles(function(articles){
+console.log(articles)
+}, lang[i].lang);
+}
+} )
+*/
+
+// PHASE 2
+
+function getCurrentCountForPreviousTop10 (callback, top10){
+  var instream = fs.createReadStream('asd3');
+  var outstream = new stream;
+  var rl = readline.createInterface(instream, outstream);
+  var topResults = [];
+  var lastValueOfTop10=0;
+  rl.on('line', function(line) {
+    var obj=parseLine(line)
+
+    for(var i=0; i<top10.length; i++)
+    if(top10[i].name === obj.name && top10[i].domain === obj.domain){
+      top10[i].count2=obj.count
+      top10[i].gain_loss = top10[i].count/top10[i].count2
+    }
+  });
+  rl.on('close', function() {
+    callback(top10)
+  });
+}
+
+/*
+// 1. Detect the top 10 pages from day 1. Calculate their gains and losses in traffic by comparing them to day 2.
+getTopArticles(function(articles){
+  getCurrentCountForPreviousTop10(function(top10){
+    console.log(top10)
+  },articles)
+})
+*/
