d50c7b78d3c9efe53880cd2bd5151c86c59b2804
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-13 10:08:16 -0500

    first Tests

--- day2/deluxe-cli/to_do_spec.js
+++ day2/deluxe-cli/to_do_spec.js
@@ -20,15 +20,27 @@ describe("Test toDo.js", function() {
   it("creating new task from blank", function() {
     child_process.execSync('node toDo.js add Do the dishes');
     var stdout = runAndCleanStdout('node toDo.js show')
-    console.log(stdout);
     expect(stdout.length).toBe(1);
+    expect(stdout[0]).toEqual("Task #1 Priority 1: Do the dishes")
   });
 
-
-  function runAndCleanStdout(cmd){
-    var stdout = child_process.execSync(cmd, {encoding:'utf-8'});
-    stdout = stdout.split(/\r\n|\r|\n/);
-    return stdout.splice(0, 1)
-  }
+  it("creating many tasks, with priority flags", function() {
+    child_process.execSync('node toDo.js add Do the dishes');
+    child_process.execSync('node toDo.js add Fix tv --priority 2');
+    child_process.execSync('node toDo.js add Call the internet guy -p 3');
+    var stdout = runAndCleanStdout('node toDo.js show')
+    expect(stdout.length).toBe(3);
+    expect(stdout[0]).toEqual("Task #1 Priority 1: Do the dishes")
+    expect(stdout[1]).toEqual("Task #2 Priority 2: Fix tv")
+    expect(stdout[2]).toEqual("Task #3 Priority 3: Call the internet guy")
+  });
 
 });
+
+
+function runAndCleanStdout(cmd){
+  var stdout = child_process.execSync(cmd, {encoding:'utf-8'});
+  stdout = stdout.split(/\r\n|\r|\n/);
+  stdout.splice(-1, 1)
+  return stdout
+}
