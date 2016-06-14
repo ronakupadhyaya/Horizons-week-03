cd5277c60ee953d3ef8c8156974accc386075c6b
quidkid <isabella.gong@gmail.com>
2016-06-14 18:59:52 -0400

    commit

--- day2/trello-csv/trello-csv.js
+++ day2/trello-csv/trello-csv.js
@@ -1,5 +1,5 @@
-var TRELLO_KEY = 'YOUR TRELLO KEY HERE';
-var TRELLO_TOKEN = 'YOUR TRELLO KEY HERE';
+var TRELLO_KEY = '63aa587ffd2bc71922dbbbdf72958ded';
+var TRELLO_TOKEN = 'a347b3f17d5ca9479ee565757c0e5a80661b163dd7e2e67aaa7af535e4cd73f7';
 
 var fs = require('fs');
 var csv = require('csv');
@@ -21,8 +21,8 @@ program.parse(process.argv);
 // - The second argument should be the csv file
 // ex. var board_id = program.args[0];
 // YOUR CODE HERE
-var board_id;
-var csv_fname;
+var board_id = program.args[0];
+var csv_fname = program.args[1];
 
 // 2. upload functionality - read csv and upload to Trello.
 // Here's some example code for reading CSV files.
@@ -32,6 +32,17 @@ var uploadToTrello = function(board_id, csv_fname) {
   csv.parse(csvData, { columns: true}, function(err, data){
     console.log(data);
     // YOUR CODE HERE
+    var returnArr = [];
+    for (var i = 0; i < data.length; i++) {
+    	_.forEach(data[i], function(value, key) {
+    		returnArr.push([key, value]);
+    	})
+    }
+
+    var newObj = _.groupBy(returnArr,, function(array) {
+    	return array[0];
+    })
+
   });
 };
 
@@ -41,5 +52,13 @@ var downloadFromTrello = function(boardId) {
 };
 
 // This line is here for demo purposes, you should delete it
-// when you get started!
-uploadToTrello(null, 'sample.csv');
+// // when you get started!
+ uploadToTrello(null, 'sample.csv');
+
+ var newArr = [["b", 2],["a", 1],["c", 3], ["b", 3], ["b",9]];
+ var newObj = _.groupBy(newArr,function(array) {
+ 	return array[0];
+ });
+ console.log(newObj);
+
+
