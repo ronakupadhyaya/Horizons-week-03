f33b6fc56c1aa1d8eed9e76eaa2dc02b319c3b75
Adam He <shuaihe2018@u.northwestern.edu>
2016-06-14 19:25:23 -0400

    day 2 - trello

--- day2/trello-csv/trello-csv.js
+++ day2/trello-csv/trello-csv.js
@@ -14,7 +14,9 @@ var program = require('commander');
 // Use commander to parse the --upload -u, --download -d flags
 // ex. program.option('-u, --upload', 'Upload CSV'); -> program.upload = true
 
-program.parse(process.argv);
+program.option('-u, --upload', 'Upload CSV')
+  .option('-d, --download', 'Download CSV')
+  .parse(process.argv);
 
 // Remaining arguments after flags are stored in program.args
 // - The first argument should be board id
@@ -32,32 +34,68 @@ var uploadToTrello = function(board_id, csv_fname) {
   csv.parse(csvData, {
     columns: true
   }, function(err, data) {
-
+    console.log(data);
     var lists = {};
     for (var l in data) {
       for (var i in data[l]) {
+        console.log(data[l][i]);
         lists[i] = lists[i] || [];
         lists[i].push(data[l][i]);
       }
     }
+    console.log(lists);
 
-    for (var i in lists) {
-      trello.addListToBoard(board_id, i, function(trelloList) {
-        for (var card in lists[i]) {
-          trello.addCard(lists[i][card], '', trelloList.id, function() {
+    // _.mapObject(lists, function(cards, listName) {
+    //   trello.addListToBoard(board_id, listName, function(a, trelloList) {
+    //
+    //     cards.forEach(function(cardName) {
+    //       trello.addCard(cardName, '', trelloList.id, function(a, cardData) {});
+    //     });
+    //   });
+    // });
 
-          });
-        }
+    trello.addListToBoard(board_id, l, function(err, trelloList) {
+      cards.forEach(function(cardName) {
+        trello.addCard(cardName, '', trelloList.id, function(err, cardData) {});
       });
-    }
+    });
+
   });
 };
 
 // 3. download functionality - read trello data and output to csv
-var downloadFromTrello = function(boardId) {
-  var lists = trello.getListOnBoard(boardId, cb);
-};
+// var downloadFromTrello = function(boardId) {
+//   trello.getListsOnBoard(bId, function(err, lists) {
+//
+//     var cardsByListName = {};
+//     lists.forEach(function(list) {
+//       trello.getCardsOnList(list.id).then(function(cards) {
+//         cards = cards.map(function(card) {
+//           return card.name;
+//         });
+//         cardsByListName[list.name] = cards;
+//         if (_.keys(cardsByListName).length === lists.length) {
+//
+//           console.log(_.zip.apply(null, _.values(cardsByListName)));
+//           // console.log(_.zip());
+//           csv.stringify(_.zip.apply(null, _.values(cardsByListName)), {
+//               header: true,
+//               columns: _.keys(cardsByListName)
+//             },
+//             function(err, output) {
+//               // save output to csv
+//               console.log(output);
+//               fs.writeFileSync(fName, output);
+//             });
+//         }
+//
+//       }, console.log);
+//     });
+//
+//   });
+// };
 
 // This line is here for demo purposes, you should delete it
 // when you get started!
+// '575ac08e82af25b4cb577ae0'
 uploadToTrello(null, 'sample.csv');
