3acbdef2cc04cd87b3a1ddb12efc0936a59170dd
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-14 16:49:58 -0500

    Creating + Showing posts

--- day3/guestbook/app.js
+++ day3/guestbook/app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cookieParser = require('cookie-parser');
 var routes = require('./routes/index');
-
 
 var app = express();
 
@@ -16,12 +12,12 @@ var exphbs = require('express-handlebars');
 app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(cookieParser());
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- day3/guestbook/routes/index.js
+++ day3/guestbook/routes/index.js
@@ -1,22 +1,31 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
+var jsonfile = require('jsonfile')
+var file = 'data.json'
+var posts = jsonfile.readFileSync(file)
 
+
+// LOGIN
 //this will respond to http://127.0.0.1:3000/login
 router.get('/login', function(req, res) {
   res.render('login', { title: 'Log In' });
 });
 
-
 router.post('/login', function(req, res) {
-  //  console.log(req.body);
-    res.cookie('username', req.body.username).redirect('/posts');
+  res.cookie('username', req.body.username).redirect('/posts');
 });
 
+// POSTS
 router.get('/posts', function(req, res) {
   res.render('posts', { title: 'Posts' });
+  console.log(posts)
 });
 router.get('/posts/new', function(req, res) {
-  res.render('post_form', { title: 'New Post' });
+  if (req.cookies && req.cookies.username){
+    res.render('post_form', { title: 'New Post' });
+  }
+  else{ console.log("not logged") }
 });
 
 router.post('/posts', function(req, res) {
@@ -26,10 +35,10 @@ router.post('/posts', function(req, res) {
     title: req.body.title,
     text: req.body.text
   }
+  posts.push(post);
+  jsonfile.writeFileSync(file, posts);
   console.log(post)
   res.redirect('/posts')
-  //res.render('posts', { title: 'Posts' });
-
 });
 
 
