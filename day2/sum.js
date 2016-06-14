88d7e2968b3f72aed59cb63db3822e69c192a50b
Lucas <lmandelbaum1@babson.edu>
2016-06-14 11:05:24 -0400

    success

--- day2/sum.js
+++ day2/sum.js
@@ -1,40 +1,32 @@
-// In this exercise we will build a command line utility for
-// summing numbers.
-//
-// sum.js should take optional command line arguments, parse the
-// arguments into numbers and return their sum. Use
-// **`process.argv`** to read command line arguments.
-//
-// ex. node sum.js 1 2
-// > 3
-// ex. node sum.js 2 3 4 5
-// > 14
-// ex. node sum.js 2 -5 3
-// > 0
-//
-// If no command line arguments are specified, you should ask the
-// user for 2 numbers and print their sum.
-//
-// ex. node sum.js
-// > Enter first number?
-// > 1
-// > Enter second number?
-// > 4
-// > 5
-//
 
-// Example code for reading command line arguments:
-console.log('Command line arguments', process.argv.slice(2));
+// var numb = 0;
+// var sum = function(n) {
+// 	numb += n
+// }
+// sum(processArgv);
+// return numb;
 
-// Example code for getting input from the user
-var readline = require('readline');
+// var readline = require('readline');
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// var rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-rl.question("Hi! What's your name? ", function(name) {
-  console.log('Nice to meet you', name);
-  rl.close();
-});
+// rl.question("Hi! What's your name? ", function(name) {
+//   console.log('Nice to meet you', name);
+//   rl.close();
+// });
+var count = 0;
+var nums = [];
+var x = process.argv;
+x.forEach(function(item) {
+	if(!isNaN(item)) {
+		var y = parseInt(item);
+		nums.push(y)
+	}
+})
+nums.forEach(function(item) {
+	count += item;
+})
+console.log(count)
