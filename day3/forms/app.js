49cc7bf7cab83e9794f9ee96a7d6e7fe72b175d0
spark4 <spark4@wellesley.edu>
2016-06-15 16:34:02 -0400

    completed

--- day3/forms/app.js
+++ day3/forms/app.js
@@ -32,16 +32,48 @@ app.get('/', function(req, res){
 // This is the endpoint that the user loads to register.
 // It contains an HTML form that should be posted back to
 // the server.
-app.get('/register', function(req, res){
-  // YOUR CODE HERE
-  res.render('register');
+app.get('/register', function(req, res) {
+  var date = new Date();
+
+  res.render('register', {
+    date: date.toDateString()
+  });
 });
 
 // ---Part 2: Validation---
 // Write a function that takes a request object and does
 // validation on it using express-validator.
+
+// document.getElementById('#regdate').value = new Date().toDateInputValue();
+// $(document).ready( function() {
+//     $('#regdate').val(new Date().toDateInputValue());
+// });​
+
 function validate(req) {
-  req.checkBody('firstName', 'Invalid firstName').notEmpty();
+  req.checkBody('fname', 'First name required').notEmpty();
+  req.checkBody('lname', 'Last name required').notEmpty();
+  req.checkBody('password', 'Password required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+  req.checkBody('password', 'Password required').notEmpty();
+  req.checkBody('lname', 'Last name required').notEmpty();
+  req.checkBody('gender', 'Gender required').notEmpty();
+  req.checkBody('newsletter', 'Newsletter sign-up required').notEmpty();
+  req.checkBody('bio', 'Bio required').notEmpty();
+  req.checkBody('regdate', 'Date required').notEmpty();
+
+  if(req.body.minitial){
+    req.checkBody('minitial', 'Middle Initial should be 1 letter long').isLength({min:1, max:1});
+  };
+  if(req.body.bMonth) {
+    req.checkBody('bMonth', 'Invalid birthday month').isInt({min:1, max: 12});
+  };
+  if(req.body.bDay) {
+    req.checkBody('bDay', 'Invalid birthday day').isInt({min: 1, max: 31});
+  };
+  if (req.body.bYear){
+    req.checkBody('bYear', 'Invalid birthday year').isInt({min:0});
+  };
+  // req.checkBody()
 }
 
 // ---Part 3: Render errors and profile---
@@ -53,13 +85,18 @@ app.post('/register', function(req, res){
   // Get errors from express-validator
   var errors = req.validationErrors();
   if (errors) {
-    res.render('register', {errors: errors});
+    res.render('register', {
+      errors: errors,
+    date: date.toDateString()
+  });
   } else {
     // YOUR CODE HERE
     // Include the data of the profile to be rendered with this template
     res.render('profile');
   }
 });
+
+
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
