d9a732d8838e3debd2d5a1decdb44402501fba38
Julian Mullins <mullinsjulian@gmail.com>
2016-06-15 10:11:28 -0400

    express

--- day3/express_echo/app.js
+++ day3/express_echo/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 
 // Let's create a new express app
 var app = express();
+var port = 3000;
 
 // Example route:
 // This creates an Express route at http://localhost:3000
@@ -16,5 +17,23 @@ app.get('/', function(request, response) {
 
 // YOUR CODE HERE
 
+app.get('/hello', function(request, response) {
+  var name = request.query.name;
+  if(! name) {
+  	name = "mysterious stranger";
+  }
+  response.send('Hello there, ' + name);
+});
+
+// app.use('/boards', function(request, response) {
+//   var resp = "";
+//   resp += "Query: " + JSON.stringify(request.query) + '\n';
+//   resp += 'Method: ' + request.method() + '\n';
+//   resp += "Path: " + request.path + '\n';
+//   resp += "Original URL: " + request.originalURL + '\n';
+//   response.send(resp);
+// });
+
 // Start the server listening on port 3000.
-app.listen(3000);
+app.listen(port);
+console.log("Express started on port " + port);
