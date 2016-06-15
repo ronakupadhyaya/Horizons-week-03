6ff1192d236b33ad1de99d30cafc6658ffc0b753
Cristian Llop <cllop1@swarthmore.edu>
2016-06-15 09:07:32 -0400

    some toDo, complete trello-csv

--- day2/deluxe-cli/toDo.js
+++ day2/deluxe-cli/toDo.js
@@ -40,7 +40,13 @@ program.command('add')
   .action(addTask);
 
 // YOUR CODE HERE for "Show" its action must call showTasks
+program.command('Show')
+  .description("Show Tasks")
+  .action(showTask);
 // YOUR CODE HERE for "Delete" its action must call deleteTask
+program.command('Delete')
+  .description("Delete Tasks")
+  .action(deleteTask);
 
 // ---Flags---
 // We will need two flags on our program. These will take values and convert them
@@ -64,6 +70,8 @@ program
 
 // Second one will be '--priority' or '-p', that will specify a priority for our task.
 // YOUR CODE HERE for "--priority and -p"
+program
+  .option('-p, --priority <n>', 'Specify priority of task', parseInt);
 
 // Arguments
 // This line is part of the 'Commander' module. It tells them (Commander) to process all the
--- day2/trello-csv/trello-csv.js
+++ day2/trello-csv/trello-csv.js
@@ -1,5 +1,5 @@
-var TRELLO_KEY = 'YOUR TRELLO KEY HERE';
-var TRELLO_TOKEN = 'YOUR TRELLO KEY HERE';
+var TRELLO_KEY = "2dc18a58ad55028b87b77fe7942e6ad2";
+var TRELLO_TOKEN = "1dbda8ff8b8a63d9c1c7bcda29afc4b527ee7f34dde00d8d9a7dce52537522e8";
 
 var fs = require('fs');
 var csv = require('csv');
@@ -13,33 +13,91 @@ var program = require('commander');
 // 1. parse cmdline args
 // Use commander to parse the --upload -u, --download -d flags
 // ex. program.option('-u, --upload', 'Upload CSV'); -> program.upload = true
-
+program.option('-u, --upload', 'Upload CSV');
+program.option('-d, --download', 'Download CSV');
 program.parse(process.argv);
 
 // Remaining arguments after flags are stored in program.args
 // - The first argument should be board id
 // - The second argument should be the csv file
 // ex. var board_id = program.args[0];
-// YOUR CODE HERE
-var board_id;
-var csv_fname;
+var board_id = program.args[0];
+var csv_fname = program.args[1];
 
 // 2. upload functionality - read csv and upload to Trello.
 // Here's some example code for reading CSV files.
 var uploadToTrello = function(board_id, csv_fname) {
   var csvData = fs.readFileSync(csv_fname).toString();
-
   csv.parse(csvData, { columns: true}, function(err, data){
-    console.log(data);
-    // YOUR CODE HERE
+  	//console.log(data);
+    for(var key in data[0]){
+    	//console.log(key);
+    	debugger;
+    	trello.addListToBoard(board_id, key, function(error, list){
+    		if(error){
+    			console.log(error);
+    		} else{
+    			var listId = list.id;
+    			var name = list.name;
+    			// //console.log(key);
+    			// console.log(list.name);
+
+    			
+    			for(var i = 0; i < data.length; i++){
+    				console.log(data[i][name]);
+    				if(data[i][name] !== ""){
+    					trello.addCard(data[i][name], "I'm a card", listId, function(error, card){
+			    			if(error){
+			    				console.log(error);
+			    			} else {
+			    				console.log("success");
+			    			}
+			    		});
+    				}
+		    	}
+    		}
+    	});
+    }
   });
 };
 
 // 3. download functionality - read trello data and output to csv
 var downloadFromTrello = function(boardId) {
-  // YOUR CODE HERE
-};
 
-// This line is here for demo purposes, you should delete it
-// when you get started!
-uploadToTrello(null, 'sample.csv');
+	var input = [];
+ 	trello.getListsOnBoard(boardId, function(error, result){
+	  	if(error){
+	  		console.log(error);
+	  	} else{
+	  		_.map(result, function(list){
+	  			var temp = [];
+		  		temp.push(list.name);
+		  		var listId = list.id;
+		  		trello.getCardsOnList(listId, function(err, cards){
+		  			_.map(cards, function(card){
+		  				console.log(card.name);
+		  				temp.push(card.name);
+		  			});
+		  			input.push(temp);
+		  			if(input.length === result.length){
+				  		console.log(input);
+				  		var stringified = _.zip.apply(null, input);
+				  		csv.stringify(stringified, function(err, output){
+				  			//console.log(output);
+			  			fs.writeFileSync('file.csv', output);
+			  			});
+	  				}
+		  		});
+	  		});
+	  		
+	  	}
+	  	
+	  });
+	};
+
+if(program.upload){
+	uploadToTrello(board_id, csv_fname);
+}
+if(program.download){
+	downloadFromTrello(board_id);
+}
