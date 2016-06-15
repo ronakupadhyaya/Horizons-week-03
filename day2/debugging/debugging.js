f188809a7e6a021e5695034e2fc120100cbc7578
AndyMartin93 <amarti40@villanova.edu>
2016-06-15 09:08:30 -0400

    debugging

--- day2/debugging/debugging.js
+++ day2/debugging/debugging.js
@@ -9,8 +9,9 @@
 // ex. replaceAll('bbbb', 'a', 'b') -> 'bbbb'
 // ex. replaceAll('x aa b x', 'a', 'b') -> 'x bb b x'
 function replaceAll(source, letter, replacement) {
+  debugger
   var ret = '';
-  for (var i = 0; i < source; i++) {
+  for (var i = 0; i < source.length; i++) {
     if (source[i] === letter) {
       ret += replacement;
     } else {
