af6cdab821f2a5f039d87ca6b2fb5f4da88e62a2
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-06-15 07:45:13 -0400

    wip

--- day3/forms/app.js
+++ day3/forms/app.js
@@ -2,9 +2,26 @@
 
 var express = require('express');
 var app = express();
-var hbl = require('handlebars');
-// var validate = require('express-validation');
 var fs = require('fs');
+var path = require('path');
+
+// Set up handlebar templates
+var exphbs = require('express-handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Enable form validation with express validator.
+var expressValidator = require('express-validator');
+app.use(expressValidator());
+
+// Enable POST request body parsing
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // DATA
 var registered = false;
@@ -31,29 +48,32 @@ var isValidData = function() {
   return true;
 };
 
-// TEMPLATES
-
-// YOUR CODE HERE
-var registrationTemplate = hbl.compile(fs.readFileSync('register.hbl').toString());
-var profileTemplate;
-
 // ROUTES
-
-// YOUR CODE HERE
-app.get('/register', function(req, res){
-  // Compile registration template
-  res.send(registrationTemplate(registrationData));
+app.get('/', function(req, res){
+  res.redirect('/register');
 });
 
+// GET /register
+// This is the endpoint that the user loads to register.
+// It contains an HTML form that should be posted back to
+// the server.
+app.get('/register', function(req, res){
+  // YOUR CODE HERE
+  res.render('register');
+});
+
+// Write a function that
+function isValid(data) {
+  // YOUR CODE HERE
+  return false;
+}
+
+// POST /register
+// This is the endpoint that the user hits when they submit
+// the registration form.
 app.post('/register', function(req, res){
-  // Complete
-  console.log(req.body);
-  if (isValidData(req.body)) {
-    
-    res.send('aight');
-  } else {
-    req.body.error = ""
-  }
+  // YOUR CODE HERE
+  res.render('register');
 });
 
 app.get('/profile', function(req, res){
