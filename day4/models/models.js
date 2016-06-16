8c50bd516af6eb825deb032d8321ff8e8c4fb349
Tyler <tylerscott.sullivan@outlook.com>
2016-06-16 14:07:45 -0400

    Rawr

--- day4/models/models.js
+++ day4/models/models.js
@@ -4,10 +4,38 @@ mongoose.connect(require('./connect'));
 
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
 
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
-});
+var kitty = new Cat({name: 'Mr Whiskers', furColor: 'calico'});
+var tiny = new Cat({name: 'Tiny', furColor: 'orange'});
+var tom = new Cat({name: 'Tom', furColor: 'grey'});
+
+kitty.save(function(error){
+	if(error) {
+		console.log(error);
+	} else {
+		console.log("MeowK");
+	}
+})
+
+tiny.save(function(error){
+	if(error) {
+		console.log(error);
+	} else {
+		console.log("MeowT");
+	}
+})
+
+tom.save(function(error){
+	if(error) {
+		console.log(error);
+	} else {
+		console.log("MeowJ");
+	}
+})
+
+// Cat.find(function(error, cats) {
+//   if (error) {
+//     console.log("Can't find cats", error);
+//   } else {
+//     console.log('Cats', cats);
+//   }
+// });
