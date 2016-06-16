d33c1044feb87b89140eb7c61e5514695dc9d62b
Christopher Mak <christophermak777@gmail.com>
2016-06-16 14:09:08 -0400

    Week 3 Day 4 Exercises

--- day4/models/models.js
+++ day4/models/models.js
@@ -2,9 +2,33 @@
 var mongoose = require('mongoose');
 mongoose.connect(require('./connect'));
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+var Cat = mongoose.model('Cat', {
+	name: {
+	type: String,
+	required: true
+	}, 
+	furColor: String
+})
+
+var crookshanks = new Cat({name: "Crookshanks",furColor: "black"});
+var bigglesworth = new Cat({name: "Mr.Bigglesworth",furColor: "white"});
+var empurress = new Cat({name: "Empurress",furColor: "calico"});
+crookshanks.save();
+bigglesworth.save();
+empurress.save();
 
 Cat.find(function(error, cats) {
+  if (error) {
+    console.log("Can't find cats", error);
+  } else {
+    console.log('Cats', cats);
+  }
+});
+
+Cat.findOne({
+	name: "Mr.Bigglesworth"
+},
+	function(error, cats) {
   if (error) {
     console.log("Can't find cats", error);
   } else {
