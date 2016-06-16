027b30ebe0b417f2848f695658bf286bbc8891f0
spark4 <spark4@wellesley.edu>
2016-06-16 11:40:35 -0400

    boop

--- day4/partials/app.js
+++ day4/partials/app.js
@@ -4,18 +4,31 @@ var exphbs  = require('express-handlebars');
 
 var app = express();
 
-// when you see a template, assume that it has a .hbs name
+// .engine below tells us that when you see a template, 
+// assume that it has a .hbs name
 app.engine('hbs', exphbs({
 	'extname':'hbs',
-	'defaultLayout': 'main'
+	// 'defaultLayout': 'main'
+	// we did not make a layouts folder
+	// but this folder would go in views
+	// AND it would be called main.hbs
 }));
+
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function (req, res) {
-    res.render('index.hbs');
+	// you render the text in here 
+	// into {{{body}}}
+    res.render('index');
+});
+
+app.get('/other', function (req, res) {
+	// you render the text in here 
+	// into {{{body}}}
+    res.render('other');
 });
 
 
