d54068c5374360f542626c0dfaff3bacfb4b9f44
AustinHawk <austinhawkins70@yahoo.com>
2016-06-16 11:39:12 -0400

    inline stuff

--- day4/models/models.js
+++ day4/models/models.js
@@ -11,3 +11,11 @@ Cat.find(function(error, cats) {
     console.log('Cats', cats);
   }
 });
+
+var crookshanks = new Cat({name: 'Crookshanks', furColor:'Black'})
+var bigglesworth = new Cat({name:'Mr. Bigglesworth', furColor:'White'})
+var empurress = new Cat({name:'Empurress', furColor:'Calico'})
+
+crookshanks.save();
+bigglesworth.save();
+empurress.save();
