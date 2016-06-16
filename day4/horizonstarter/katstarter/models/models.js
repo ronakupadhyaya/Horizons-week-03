cfffe0590eeaad4827bf635147033a2f1512b4f8
bw-12 <brendan_woo@brown.edu>
2016-06-16 18:38:50 -0400

    asdasdasd

---
+++ day4/horizonstarter/katstarter/models/models.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var connect = require('./connect');
+
+mongoose.connect(connect);
+
+module.exports = {
+	project: mongoose.model('Project', {
+		title: {
+			type: String,
+			required: true
+		}
+		cat: {
+			type: String,
+			required: true
+		}
+		goal: {
+			type: Number,
+			required: true
+		}
+		total: {
+			type: Number,
+			required: false
+		}
+		start: {
+			type: String,
+			required: true
+		}
+		end: {
+			type: String
+			required; true
+		}
+	})
+}
--- day4/horizonstarter/katstarter/routes/index.js
+++ day4/horizonstarter/katstarter/routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
+var models = require('../models/models');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Katssnipppppp' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Katssnipppppp' });
+// });
+
+router.get('/katstarter', function(req, res) {
+	models.project.find(function(error, mangoProjects) {
+		res.render('projects', {
+			'projects': mangoProjects
+		})
+	})
 });
+
+router.get('/new', function(req, res) {
+	res.render('new');
+});
+
+router.post('/new', function(req, res) {
+	var p = new models.project({title: req.body.title,
+								cat: req.body.cat,
+								goal: req.body.goal,
+								total: 0,
+								start: req.body.start,
+								end: req.body.end});
+	p.save(function(err, mangoProjects) {
+		if(err) {
+			res.status(400).send("Error reading project");
+		} else {
+			res.redirect('/katstarter/' + mangoProjects._id);
+		}
+	})
+});
+
+router.get('/katstarter/:id', function(req, res) {
+	models.project.findById(req.params.id, function(error, mangoProjects) {
+		if(error) {
+			res.status(400).send("Error reading project");
+		} else if(! mangoProjects) {
+			res.status(404).send('No such project: ' + req.params.id);
+		} else {
+			res.render('singleProject', {
+				'project': mangoProjects
+			})
+		}
+	});
+});
+
+
+
 
 module.exports = router;
