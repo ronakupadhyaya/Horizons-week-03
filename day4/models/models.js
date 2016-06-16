b71773b36b47eb052264485ffe875a08848a35dc
Serena Woodward <smew820@gmail.com>
2016-06-16 11:08:53 -0400

    I don't think i'm syncing things correctly.

--- day4/models/models.js
+++ day4/models/models.js
@@ -2,12 +2,61 @@
 var mongoose = require('mongoose');
 mongoose.connect(require('./connect'));
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+var Cat = mongoose.model('Cat', {
+	name: {
+		type: String,
+		required: true
+		}, 
+	furColor: String
+}); 
 
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
-});
+var crookshanks = new Cat({name: "Crookshanks", furColor:"Black"});
+var mr_bigglesworth = new Cat ({name: "Mr. Bigglesworth", furColor: "White"}); 
+var empurress = new Cat ({name: "Empurress", furColor: "Calico"});
+var purrfect = new Cat({ name: "purrfect", furColor: "white"}); 
+
+crookshanks.save(); 
+mr_bigglesworth.save(); 
+empurress.save();
+purrfect.save();  
+
+var namelessCat = new Cat(); 
+
+namelessCat.save(function(error){ 
+if(error){
+		console.log("could not save", error);
+		}
+		else {
+			console.log("meow");
+		} 
+	});
+
+
+
+
+
+
+
+
+
+
+// crookshanks.save(function(error){
+// 	if(error){
+// 		console.log("could not save", error);
+// 		}
+// 		else {
+// 			console.log("meow");
+// 		} 
+	// });  
+
+// Cat.findOne( {
+// 	"name": "purrfect" 
+// },
+// 	function(error, cats){
+//   if (error) {
+//     console.log("Can't find cats", error);
+//   } else {
+//     console.log('Cats', cats);
+//   }
+// });
+
