8669789fc39443e4c3286ce55a532f35c73bab79
AndyMartin93 <amarti40@villanova.edu>
2016-06-16 14:07:58 -0400

    model

--- day4/models/models.js
+++ day4/models/models.js
@@ -1,13 +1,66 @@
 // First let's set up our MongoDb connection
 var mongoose = require('mongoose');
 mongoose.connect(require('./connect'));
+//Cat constructor
+var Cat = mongoose.model('Cat', {
+	name: String, 
+	furColor: String
+});
+// Make cats
+var cat = new Cat ({
+	name: "cat", 
+	furColor: "white"
+});
+var floyd = new Cat ({
+	name: "floyd", 
+	furColor: "black and white"
+});
+var mcPussington = new Cat ({
+	name: mcPussington, 
+	furColor: "black on black"
+});
+var namelessCat = new Cat ();
+namelessCat.save(function(error) {
+	if (error) {
+		console.log('Error', error)
+	}
+	else {
+		console.log('meow meow meow')
+	}
+});
+//Save my cats
+cat.save(function(error) {
+	if (error) {
+		console.log('Error', error)
+	}
+	else {
+		console.log('meow meow meow')
+	}
+});
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+floyd.save(function(error) {
+	if (error) {
+		console.log('Error', error)
+	}
+	else {
+		console.log('meow meow meow')
+	};
+});
 
+mcPussington.save(function(error) {
+	if (error) {
+		console.log('Error', error)
+	}
+	else {
+		console.log('meow meow meow')
+	};
+});
+//Find my cats by searching the database
 Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
+	console.log('here');
+	if (error) {
+	   console.log("Can't find cats", error);
+	} else {
+	   console.log('Cats', cats);
+	}
 });
