ddbbf79191558a2baa757307577726b016be5fba
Uriel Baez <isabaez@seas.upenn.edu>
2016-06-16 11:06:44 -0400

    Warm up

--- day4/models/models.js
+++ day4/models/models.js
@@ -4,6 +4,14 @@ mongoose.connect(require('./connect'));
 
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
 
+var crook = new Cat({name: "Crookshanks", furColor: "Black"})
+var biggles = new Cat({name: "Mr.Bigglesworth", furColor: "White"})
+var empurr = new Cat({name: "Empurress", furColor: "Calico"})
+
+// crook.save();
+// biggles.save();
+// empurr.save();
+
 Cat.find(function(error, cats) {
   if (error) {
     console.log("Can't find cats", error);
