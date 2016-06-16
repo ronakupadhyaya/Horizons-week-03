4c4e794425b1c8b636e5c2a3f13b156297f33b29
Virginia Van Keuren <virginiavankeuren2017@u.northwestern.edu>
2016-06-16 14:18:07 -0400

    created cat database with Moose

--- day4/models/models.js
+++ day4/models/models.js
@@ -1,13 +1,90 @@
 // First let's set up our MongoDb connection
+//HOW TO WRITE TO MONGODB WITH MONGOOSE USING SAVE
 var mongoose = require('mongoose');
 mongoose.connect(require('./connect'));
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+//CREATE A NEW COLLECTION (EX. CHANGE CAT TO MOOSE FOR NAME)
+var Cat = mongoose.model('Cat', {
+  name: {
+    type: String,
+    required: true,
+  },
+  furColor: String
+});
 
-Cat.find(function(error, cats) {
+var mrWhisker = new Cat({name: 'Mr Whiskers', furColor: 'calico'});
+var crookshanks = new Cat({name: 'Crookshanks', furColor: 'black'});
+var mrBigglesworth = new Cat({name: 'Mr. Bigglesworth', furColor: 'white'});
+var empurress = new Cat({name: 'Empurress', furColor: 'calico'});
+
+//HOW TO SAVE A NEW COLLECTION?
+// new Cat().save(function(error){
+//   if (error) {
+//     console.log("Can't find cats", error);
+//   } else {
+//     console.log('Cats', cats);
+//   }
+// })
+
+//BRINGS UP THE DOCUMENTS (RESPONSES) OF THE COLLECTION IN ONE ARRAY, MULTIPLE OBJECTS
+//FINDER FUNCTION
+Cat.findOne({
+  name: "Mr. Bigglesworth" //same as mlab interface search function
+},function(error, cats) {
   if (error) {
     console.log("Can't find cats", error);
   } else {
-    console.log('Cats', cats);
+    console.log('meow', cats);
   }
 });
+
+Cat.find({
+},function(error, cats) {
+  if (error) {
+    console.log("Can't find cats", error);
+  } else {
+    console.log('meow', cats);
+  }
+});
+
+//HOW TO ADD A DOCUMENT TO COLLECTION
+// var kitty = new Cat({name: 'Kitty', furColor: 'blue'})
+// kitty.save(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
+
+//NAMELESS CAT
+//THIS WON'T WORK BECAUSE UNDER CAT, WE MADE NAME TO BE REQUIRED
+// var nameless = new Cat()
+// nameless.save(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
+
+//DO THESE ONLY ONCE, THEN COMMENT OUT
+// mrWhisker.save(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
+//
+// crookshanks.save(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
+//
+// mrBigglesworth.save(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
+//
+// empurress.save(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
--- day4/partials/app.js
+++ day4/partials/app.js
@@ -4,7 +4,8 @@ var exphbs  = require('express-handlebars');
 
 var app = express();
 
-app.engine('hbs', exphbs({extname:'hbs'}));
+app.engine('hbs', exphbs({
+  'extname':'hbs'}));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +14,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
     res.render('index.hbs');
 });
-
-
 app.listen(3000);
+
+//FROM MOOSE'S EXERCISE (VIEWS/LAYOUT FILES)
+//THIS IS THE TEMPLATE GRABBER, AND IT APPLIES TO THE GET FUNCTIONS
+// app.engine('hbs', exphbs({
+//   'extname': 'hbs',
+//   'defaultLayout': 'main'
+// }));
+// app.set('view engine','hbs');
+//
+// // THIS PULLS UP THE ACTUAL PAGE FOR localhost:3000/
+// app.get('/', function(req,res){
+//   res.render('index.hbs');
+// });
+//
+// // THIS PULLS UP THE ACTUAL PAGE FOR localhost:3000/other
+// app.get('/other', function(req,res){
+//   res.render('other.hbs');
+// })
+// app.listen(3000);
