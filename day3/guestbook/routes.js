19503021cd30ca72930c347c43cf5793412f0c14
willyoo42 <wyoo1997@gmail.com>
2016-06-16 09:08:27 -0400

    dash

--- day3/guestbook/routes.js
+++ day3/guestbook/routes.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 // We use this module to store and retrieve data.
 // data.read(): Read the latest data stored on disk.
-// data.write(data): Write the given data to disk.
+// data.save(data): Write the given data to disk.
 var data = require('./data');
 
 // ---Part 1. Login---
@@ -15,33 +15,36 @@ var data = require('./data');
 // on their browser. This function is already implemented for you to have as a model.
 // It passes the object with title : 'Log in' to the template
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Log In' });
+    res.render('login', { title: 'Log In' });
 });
 
 // POST /login: Receives the form info for the user, sets a cookie on the client
 // (the user's browser) and redirects to posts.
 router.post('/login', function(req, res) {
-  res.cookie('username', req.body.username).redirect('/posts');
+    res.cookie('username', req.body.username).redirect('/posts');
 });
 
 // ---Part 2. View Posts---
 
 // GET /posts: Renders a list of all available posts. No need to be logged in.
 // This function should render all the posts ordered descending by date if no params
-// are passed of ir called: /posts?order=descending
+// are passed of or called: /posts?order=descending
 // If the user visits the url /posts?order=ascending they should be ordered ascending
 // If the function is called with a username like /posts?username=steven, you should
 // filter all posts that aren't done by that user.
 // Hint: to get the username, use req.query.username
 
-router.get('/posts', function (req, res) {
-  // YOUR CODE HERE
-
-  // This renders the posts
-  res.render('posts', {
-    title: 'Posts',
-    posts: []
-  });
+router.get('/posts', function(req, res) {
+    var posts = data.read();
+    posts.sort(function(a, b){
+        var y = new Date(a.date)
+        var z = new Date(b.date)
+        return y - z;
+    });
+    // This renders the posts
+    res.render('posts', {
+        posts: posts
+    });
 });
 
 // ---Part 3. New post form---
@@ -49,20 +52,43 @@ router.get('/posts', function (req, res) {
 // User must be logged in to be able to visit this page.
 // Hint: if req.cookies.username is set, the user is logged in.
 router.get('/posts/new', function(req, res) {
-  // YOUR CODE HERE
+    if (req.cookies.username) {
+        res.render('post_form')
+    }
 });
 
 // ---Part 4. Create new post
 // POST /posts: This route receives the information for the new post. User must
 // be logged in to use this route. It should create a new post and redirect to
 // posts.
+
 // It should also use express-validator to check if the title and body aren't empty.
 // an example validation using express-validator is:
 // req.checkBody('email', 'Email must not be valid').isEmail();
 // Don't forget to check if there are validation errors at req.validationErrors();
 // After updating data, you should write it back to disk wih data.save()
+
+function validate(req) {
+    req.checkBody('title', 'Insert title').notEmpty();
+    req.checkBody('body', 'Insert body').notEmpty();
+}
+
 router.post('/posts', function(req, res) {
-  // YOUR CODE HERE
+    validate(req); //validates the request and returns validation errors
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('posts', { error: errors })
+    }
+    if (req.cookies.username) {
+        var renderArray = data.read()
+        req.body.date = new Date();
+        renderArray.push(req.body)
+        console.log(renderArray)
+
+        data.save(renderArray)
+
+        res.redirect('/posts');
+    }
 });
 
 module.exports = router;
