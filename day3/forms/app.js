a25f65762116643a131b403473078cd2faddff8e
Tyler <tylerscott.sullivan@outlook.com>
2016-06-15 20:09:47 -0400

    rawr

--- day3/forms/app.js
+++ day3/forms/app.js
@@ -28,13 +28,43 @@ app.get('/', function(req, res){
   res.redirect('/register');
 });
 
+
+function getMonths() {
+  var returnArray = [];
+  for (var i = 1; i < 13; i++) {
+    returnArray.push(i);
+  }
+  return returnArray;
+}
+
+function getYears() {
+  var returnArray = [];
+  for (var i = 1910; i < 2020; i++) {
+    returnArray.push(i);
+  }
+  return returnArray;
+}
+
+function getDays() {
+  var returnArray = [];
+  for (var i = 1; i < 32; i++) {
+    returnArray.push(i);
+  }
+  return returnArray;
+}
+
+
+
 // ---Part 1: GET /register---
 // This is the endpoint that the user loads to register.
 // It contains an HTML form that should be posted back to
 // the server.
 app.get('/register', function(req, res){
-  // YOUR CODE HERE
-  res.render('register');
+  res.render('register', {
+    Months: getMonths(),
+    Years: getYears(),
+    Days: getDays()
+  });
 });
 
 // ---Part 2: Validation---
@@ -42,6 +72,9 @@ app.get('/register', function(req, res){
 // validation on it using express-validator.
 function validate(req) {
   req.checkBody('firstName', 'Invalid firstName').notEmpty();
+  req.checkBody('lastName', 'Invalid lastName').notEmpty();
+  req.checkBody('password', 'Invalid password').notEmpty();
+  req.checkBody('repeat', 'Invalid matched password').notEmpty();
 }
 
 // ---Part 3: Render errors and profile---
@@ -50,14 +83,41 @@ function validate(req) {
 // the registration form.
 app.post('/register', function(req, res){
   validate(req);
-  // Get errors from express-validator
+  
   var errors = req.validationErrors();
+
+  if (req.body.password !== req.body.repeat) {
+    if (!errors) {
+      errors = [];
+    }
+    errors.push({
+      msg: "Passwords don't match"
+    })
+  }
   if (errors) {
-    res.render('register', {errors: errors});
+
+    res.render('register', {
+      Months: getMonths(),
+      Years: getYears(),
+      Days: getDays(),
+      errors: errors
+    });
   } else {
     // YOUR CODE HERE
     // Include the data of the profile to be rendered with this template
-    res.render('profile');
+    res.render('profile', {
+      registerDate: new Date().toDateString(),
+      firstName: req.body.firstName,
+      middleI: req.body.middleI,
+      lastName: req.body.lastName,
+      DOBMonth: req.body.DOBMonth, // didn't work
+      DOBDay: req.body.DOBDay, // didn't work
+      DOBYear: req.body.DOBYear, // didn't work
+      password: req.body.password,
+      gender: req.body.optionsRadios, //didn't work
+      newsletter: req.body.checkbox,
+      biography: req.body.biography
+    });
   }
 });
 
