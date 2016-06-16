5d1cc26b8fc321a08dd62515e185ca2a4182d620
Isaac Kapadia <ikapedia@umich.edu>
2016-06-16 14:08:14 -0400

    worked

--- day4/models/models.js
+++ day4/models/models.js
@@ -4,10 +4,22 @@ mongoose.connect(require('./connect'));
 
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
 
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
+var cat1 = new Cat({name: 'Crookshanks', furColor: 'black'});
+console.log(cat1);
+cat1.save(function(error) {
+	if (error) {
+		console.log('nope');
+	}
+	else {
+		console.log('good');
+	}
 });
+
+// Cat.find(function(error, cats) {
+// 	console.log('h');
+//   if (error) {
+//     console.log("Can't find cats", error);
+//   } else {
+//     console.log('Cats', cats);
+//   }
+// });
--- day4/partials/app.js
+++ day4/partials/app.js
@@ -5,14 +5,18 @@ var exphbs  = require('express-handlebars');
 var app = express();
 
 app.engine('hbs', exphbs({extname:'hbs'}));
+
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// This represents the controller in the MVC model
 app.get('/', function (req, res) {
     res.render('index.hbs');
 });
 
+// app.get('/', function(req, res) {
+// 	res.render('success.hbs');
+// });
 
 app.listen(3000);
