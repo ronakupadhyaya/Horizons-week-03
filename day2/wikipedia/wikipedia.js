31c89f909c600c79e8cd616261906c6ed5162580
Isaac Kapadia <ikapedia@umich.edu>
2016-06-16 09:09:23 -0400

    Worked on it

--- day2/wikipedia/wikipedia.js
+++ day2/wikipedia/wikipedia.js
@@ -11,16 +11,51 @@ function countLines(fileName) {
     input: input
   });
   
+  var lineArray = [];
+  var langArray = [];
   var count = 0;
   rl.on('line', function(line) {
     // This is called for each line in file
-
+    lineArray = line.split(' ');
+    langArray.push(lineArray);
     count++;
   });
+
+
   rl.on('close', function() {
     // This is called when the file is done being read finished
+    langArray = langArray.sort(function(a,b) {
+      if ([a][0] > [b][0]) {
+        return -1;
+      }
+      else {
+        return 1;
+      }
+      });
+    //debugger;
+   // console.log(langArray);
+   var currentArray = [langArray[0]];
+   for (var i = 0; i < langArray.length; i++) {
+    
+    if (langArray[i][0] === langArray[i + 1][0]) {
+      langArray[i][3] += langArray[i+1][3];
+      langArray.splice(i+1, 1);
+      i+=1;
+      }
+      else {
+        i+=1;
+      }
+   }
+
+      // if (currentArray[i][0] === langArray[i+1][0]) {
+      //   currentArray[i][3] += langArray[i+1][3];
+
+      // }
+
+
+   console.log(langArray);
     console.log('There are %s lines in file %s', count, fileName);
   });
 }
 
-countLines(__filename);
+countLines('sample.data');
