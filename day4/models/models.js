1cb4f8e518dc89aa15b42610eb57635b740e3c52
younggann <youngan@sas.upenn.edu>
2016-06-16 14:07:26 -0400

    worked on warmup stuff

--- day4/models/models.js
+++ day4/models/models.js
@@ -4,10 +4,22 @@ mongoose.connect(require('./connect'));
 
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
 
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
-});
+var callback = function(error, success) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(success);
+    }
+};
+
+var Cat1 = new Cat({name: "Crookshanks", furColor: "Black"}).save(callback);
+var Cat2 = new Cat({name: "Mr. Bigglesworth", furColor: "White"}).save(callback);
+var Cat3 = new Cat({name: "Empuress", furColor: "Calico"}).save(callback);
+
+// Cat.find(function(error, cats) {
+//   if (error) {
+//     console.log("Can't find cats", error);
+//   } else {
+//     console.log('Cats', cats);
+//   }
+// });
