78f268aea4fd2a5b7cbdbf19af2b3b443d3cde37
tconcannon <taycon@seas.upenn.edu>
2016-06-16 10:42:54 -0400

    annotated

--- day4/models/models.js
+++ day4/models/models.js
@@ -6,7 +6,15 @@ mongoose.connect(require('./connect'));
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
 
 var crookshanks = new Cat({name: 'Crookshanks', color: 'black'})
-crookshanks.save()
+crookshanks.save(function(error){
+	if(error){
+		console.log('could not save', error)
+	}
+	else{
+		console.log('meow') ////checking mechanism to verify whether it saved to mLab or 
+		//had an error
+	}
+})
 //need to type INDIVIDUAL cats to save!!!!
 var biggles = new Cat({name: 'Mr. Bigglesworth', color: 'white'})
 biggles.save()
