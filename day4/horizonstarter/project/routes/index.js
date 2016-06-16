092d49db981384f2dfe2aa0d4f16cd88909dd49b
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-06-16 17:49:23 -0400

    Demo work-in-progress.

--- day4/horizonstarter/project/routes/index.js
+++ day4/horizonstarter/project/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+var models = require('../models/models');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/projects', function(req, res) {
+  models.project.find(function(error, mongoProjects) {
+    res.render('projects', {
+      'projects': mongoProjects
+    });
+  })
+});
+
+router.get('/projects/:id', function(req, res) {
+  models.project.findById(req.params.id, function(error, mongoProject) {
+    if (error) {
+      res.status(400).send('Error reading project ' + error);
+    } else if (! mongoProject) {
+      res.status(404).send('No such project: ' + req.params.id);
+    } else {
+      res.render('singleProject', {
+        'project': mongoProject
+      });
+    }
+  });
+});
+
+router.get('/new', function(req, res){
+  res.render('new');
+});
+
+router.post('/new', function(req, res) {
+  var p = new models.project({ title: req.body.title });
+  p.save(function(error, project) {
+    if (error) {
+      res.status(400).send("Error creating project: " + error);
+    } else {
+      res.redirect('/projects/' + project._id);
+    }
+  })
+});
+
+router.get('/hello', function(req, res) {
+  res.send('Hello there!');
 });
 
 module.exports = router;
---
+++ day4/horizonstarter/project/script2.js
@@ -0,0 +1,12 @@
+var secretString = "myBankPassword??";
+
+console.log('doing stuff in script2');
+
+var x = 100;
+
+// module.exports = {
+//   secretString: secretString,
+//   x: x
+// };
+
+module.exports = secretString;
