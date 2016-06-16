36d4c650311207545f4a1d9c25c2076404cf17f2
quidkid <isabella.gong@gmail.com>
2016-06-16 14:08:03 -0400

    day 4 inline exercises

--- day4/models/models.js
+++ day4/models/models.js
@@ -3,8 +3,35 @@ var mongoose = require('mongoose');
 mongoose.connect(require('./connect'));
 
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
+// var crookshanks = new Cat({name: "Crookshanks", furColor:"Black"});
+// crookshanks.save(function(error) {
+// if (error) {
+// console.log(error) ;
+// }
+// });
+// var bigglesworth = new Cat({name: "Mr. Bigglesworth", furColor:"White"});
+// bigglesworth.save(function(error) {
+// if (error) {
+// console.log(error) ;
+// }
+// });
+// var empurress = new Cat({name: "Empurress", furColor:"Calico"});
+// empurress.save(function(error) {
+// if (error) {
+// console.log(error) ;
+// }
+// });
 
-Cat.find(function(error, cats) {
+// Cat.find(function(error, cats) {
+//   if (error) {
+//     console.log("Can't find cats", error);
+//   } else {
+//     console.log('Cats', cats);
+//   }
+// });
+
+Cat.findOne({name:"Mr. Bigglesworth"}, function(error, cats)
+	{
   if (error) {
     console.log("Can't find cats", error);
   } else {
