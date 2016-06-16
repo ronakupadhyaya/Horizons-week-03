0ce4d4609c7d22254fff66540e3825b342e79a26
JoostKamermans <JoostKamermans@Joosts-MacBook-Pro.local>
2016-06-16 10:09:49 -0400

    asdf
    asdf

--- day3/forms/app.js
+++ day3/forms/app.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+
 var fs = require('fs');
 var path = require('path');
 
@@ -28,21 +29,54 @@ app.get('/', function(req, res){
   res.redirect('/register');
 });
 
+function getYears() {
+  var ret = [];
+  for(var i = 1899; i < new Date().getFullYear(); i++){
+    ret.push(i);
+  }
+  return ret
+}
+
+function getDaysofMonth() {
+  var ret = [];
+  for(var i = 1; i < 32; i++) {
+    ret.push(i);
+  }
+  return ret;
+}
+
+
 // ---Part 1: GET /register---
 // This is the endpoint that the user loads to register.
 // It contains an HTML form that should be posted back to
 // the server.
-app.get('/register', function(req, res){
+app.get('/register', function(req, res) {
   // YOUR CODE HERE
-  res.render('register');
+  res.render('register', {
+    daysOfMonth: getDaysofMonth(),
+    years: getYears()
+  });
 });
+
+app.get('/form'), function(req, res) {
+  res.render('form');
+}
 
 // ---Part 2: Validation---
 // Write a function that takes a request object and does
 // validation on it using express-validator.
 function validate(req) {
-  req.checkBody('firstName', 'Invalid firstName').notEmpty();
+  req.checkBody('firstName', 'Fist name is required').notEmpty();
+  req.checkBody('lastName', 'Last name is required').notEmpty();
+  // req.checkBody('dobDay', 'DOB day must be a number').isInt();
+  // req.checkBody('dobMonth', 'DOB month must be a number').isInt();
+  // req.checkBody('dobYear', 'DOB year must be a number').isInt();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'password is required').notEmpty();
+
 }
+
+
 
 // ---Part 3: Render errors and profile---
 // POST /register
@@ -52,15 +86,49 @@ app.post('/register', function(req, res){
   validate(req);
   // Get errors from express-validator
   var errors = req.validationErrors();
+  var month = parseInt(req.body.dobMonth);
+  var day = parseInt(req.body.dobDay);
+  var year = parseInt(req.body.dobYear);
+  var date = new Date(year,  month -1, day);
+  var now = new Date();
+  if (now.getTime() < date.getTime()) {
+    console.log('person is born in the future');
+  
+  if(! errors) {
+    errors = [];
+  }
+  errors.push({
+    msg: "DOB cannot be in future"
+  })
+
+}
+  if (req.body.password !== req.body.password2) {
+    if (! errors) {
+      errors = [];
+    }
+    errors.push({
+      msg: "Passwords don't match"
+    })
+  }
+
+
+
+
   if (errors) {
     res.render('register', {errors: errors});
   } else {
-    // YOUR CODE HERE
-    // Include the data of the profile to be rendered with this template
-    res.render('profile');
+    res.render('profile', {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    middleInitial: req.body.middleInitial,
+    dobMonth: req.body.dobMonth,
+    dobDay: req.body.dobDay,
+    dobYear: req.body.dobYear
+  });
   }
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
+var port = 3000;
+app.listen(port, function() {
+  console.log("Example app listening on port " +port+ "!");
 });
