508b518dba8c8c620fac50e9f998bb045669871b
kitchena <kitchena@wharton.upenn.edu>
2016-06-16 14:08:13 -0400

    horizonstarter

--- day4/models/models.js
+++ day4/models/models.js
@@ -1,8 +1,16 @@
 // First let's set up our MongoDb connection
 var mongoose = require('mongoose');
 mongoose.connect(require('./connect'));
-
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
+
+var crookshanks = new Cat({name: "Crookshanks", furColor: "Black"});
+var mrBigglesworth = new Cat({name: "Mr. Bigglesworth", furColor: "White"});
+
+var empurress = new Cat({name: "Empurress", furColor: "Calico"});
+
+crookshanks.save(function(error) { });
+mrBigglesworth.save(function(error) {});
+empurress.save(function(error) {});
 
 Cat.find(function(error, cats) {
   if (error) {
