eff23f27f8ebc041d13267c0a2a9c64b2fa62a1d
aschugart <annieschugart@Annies-MacBook-Pro-2.local>
2016-06-16 12:56:52 -0400

    inline stuff

--- day4/models/models.js
+++ day4/models/models.js
@@ -4,6 +4,14 @@ mongoose.connect(require('./connect'));
 
 var Cat = mongoose.model('Cat', {name: String, furColor: String})
 
+var crook = new Cat({name: 'Crookshanks', furColor: 'Black' })
+var biggle = new Cat({name: 'Bigglesworth', furColor: 'White' })
+var empurr = new Cat({name: 'Empurress', furColor: 'Calico' })
+
+crook.save();
+biggle.save();
+empurr.save();
+
 Cat.find(function(error, cats) {
   if (error) {
     console.log("Can't find cats", error);
@@ -11,3 +19,11 @@ Cat.find(function(error, cats) {
     console.log('Cats', cats);
   }
 });
+
+Cat.find({name: 'Bigglesworth'}, function(error, biggle){
+	if (error) {
+		console.log("error")
+	} else {
+		console.log(biggle);
+	}
+})
