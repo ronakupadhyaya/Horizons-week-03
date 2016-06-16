16ce071f3b0a3f5f725c73837d9a4b63e6354440
spark4 <spark4@wellesley.edu>
2016-06-16 10:56:17 -0400

    woooo

--- day4/models/models.js
+++ day4/models/models.js
@@ -2,12 +2,42 @@
 var mongoose = require('mongoose');
 mongoose.connect(require('./connect'));
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+var Cat = mongoose.model('Cat', {name: String, furColor: String});
 
-Cat.find(function(error, cats) {
+// var Crook = new Cat({
+// 	name: "Crookshanks",
+// 	furColor: "Black"
+// });
+
+// var Bigglesworth = new Cat({
+// 	name: "Mr. Bigglesworth",
+// 	furColor: "White"
+// });
+
+// var Empurress = new Cat({
+// 	name: "Empurress",
+// 	furColor: "Calico"
+// });
+
+// Crook.save();
+// Bigglesworth.save();
+// Empurress.save();
+
+// Cat.find(function(error, cats) {
+//   if (error) {
+//     console.log("Can't find cats", error);
+//   } else {
+//     console.log('Cats', cats);
+//   }
+// });
+
+
+Cat.findOne(function(error, cats) {
   if (error) {
     console.log("Can't find cats", error);
   } else {
     console.log('Cats', cats);
   }
 });
+
+Cat.findOne();
