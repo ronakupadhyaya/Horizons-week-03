e24c64ed53c9be46cf394dd4323fedbd227ea679
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-15 21:12:18 -0500

    deal, shuffle, calculate

--- day5/blackjack/solution/routes/index.js
+++ day5/blackjack/solution/routes/index.js
@@ -4,12 +4,28 @@ var router = express.Router();
 var jsonfile = require('jsonfile');
 var file = 'data.json';
 
-var deck = [];
+
 // Posts loaded from disk
 var posts = jsonfile.readFileSync(file);
 
-var createDeck = function (){
-  var numCards=0;
+var Game = function(maze) {
+  this.deck = [];
+
+  // TODO: substract user money.
+  this.userTotal=0;
+  this.dealerTotal=0;
+  this.userBust=false;
+  this.dealerBust=false;
+  this.currentPlayerHand = [];
+  this.houseHand = [];
+
+  this.createDeck()
+  this.shuffleDeck()
+  this.deal21()
+}
+
+
+Game.prototype.createDeck = function (){
   var suit, symbol;
   for(var k=1; k<=4; k++){
     switch(k){
@@ -26,212 +42,142 @@ var createDeck = function (){
         case 12: symbol = "Q"; break;
         case 13: symbol = "K"; break;
       }
-      deck.push(new Card(suit, i, symbol));
+      this.deck.push(new Card(suit, i, symbol));
     }
   }
 }
 
+Game.prototype.shuffleDeck = function () {
+  var currentIndex = this.deck.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this.deck[currentIndex];
+    this.deck[currentIndex] = this.deck[randomIndex];
+    this.deck[randomIndex] = temporaryValue;
+  }
+}
+
+Game.prototype.calcValue = function (hand){
+  var val = 0;
+  var tempArr = hand;
+  tempArr.sort(function(a,b) { return parseFloat(a.val) - parseFloat(b.val) } );
+  for(var i=tempArr.length-1; i>=0; i--) {
+    var temp = tempArr[i];
+    if(temp.val === 1 && val <=10){temp.val = 11;}
+    else if(temp.val >=10){temp.val = 10;}
+    val += temp.val;
+  }
+  return val;
+}
+
+Game.prototype.deal21 = function () {
+  for(var i=0; i<2; i++){
+    if(this.deck.length<2)this.newDeck();
+    this.currentPlayerHand.push(this.deck.pop());
+    this.houseHand.push(this.deck.pop());
+  }
+  this.userTotal = this.calcValue(this.currentPlayerHand);
+  this.dealerTotal = this.calcValue(this.houseHand);
+
+  // TODO: hide dealers first card!!
+  //  var blackjack =true; ???
+  //  if(this.userTotal === 21 && this.dealerTotal < 21) this.gameOver(blackjack); //userWins
+  //  else if(this.dealerTotal === 21) this.gameOver(); //dealer Wiinssss.
+};
+
 router.get('/newgame', function(req, res) {
-  createDeck();
-  console.log(deck)
-//res.render('login', { title: 'Log In' });
+  var game = new Game();
+  console.log(game)
+  //res.render('login', { title: 'Log In' });
 });
 
 /*
-
-function start()
-{
-  play();
-}
-
-function play()
-{
-  var deck1 = new Deck();
-  deck1.createDeck();
-  deck1.shuffle();
-  deck1.deal21();
-  document.getElementById("hit").addEventListener("click", function(){deck1.hit();},false);
-  document.getElementById("stand").addEventListener("click", function(){deck1.stand();},false);
-  document.getElementById("replay").addEventListener("click", function(){deck1.deal21();}, false);
+hit -> deck1.hit();
+stand -> deck1.stand();
+new game -> deck1.deal21()
 }
 
 function Deck()
 {
-  this.deck = new Array();
-  this.userTotal;
-  this.dealerTotal;
-  this.money = 100;
-  this.userBust;
-  this.dealerBust;
-  this.curusrHand;
-  this.curdlrHand;
-  var userHand = document.getElementById("user-hand");
-  var dealerHand = document.getElementById("dealer-hand");
-  var userScore = document.getElementById("user-score");
-  var dealerScore = document.getElementById("dealer-score");
-  var status = document.getElementById("game-status");
-  var moneyDiv = document.getElementById("money");
-
-  this.newDeck = function newDeck()
-  {
-    this.createDeck();
-    this.shuffle();
-    status.innerHTML="NEW DECK IN PLAY!";
-  }
-
-  this.shuffle = function shuffle()
-  {
-    var randomDeck = new Array();
-    var empty = false;
-    while(!empty)
-    {
-      var randomIndex = Math.floor(Math.random()*this.deck.length);
-      randomDeck.push(this.deck[randomIndex]);
-      this.deck.splice(randomIndex, 1);
-      if(this.deck.length <=0) empty = true;
-    }
-    for(var i=0; i<randomDeck.length; i++)
-    {
-      this.deck[i] = randomDeck[i];
-    }
-  };
 
 
-  this.calcValue = function calcValue(hand)
-  {
-    var val = 0;
-    var tempArr = hand;
-    tempArr.sort(function(a,b) { return parseFloat(a.val) - parseFloat(b.val) } );
-    for(var i=tempArr.length-1; i>=0; i--)
-    {
-      var temp = tempArr[i];
-      if(temp.val === 1 && val <=10)temp.val = 11;
-      else if(temp.val >=10) temp.val = 10;
-      val += temp.val;
-    }
-    return val;
-  };
-
-  this.emptyDeck = function emptyDeck()
-  {
-    if(this.deck.length < 1) return true;
-    else return false;
-  }
-
-  this.deal21 = function deal21()
-  {
-    status.innerHTML="";
-    this.money--;
-
-    //reset all the stuff that needs to be reset if the game is being replayed
-    money.innerHTML= "Money: " + this.money;
-    dealerHand.innerHTML="<h2>Dealer Hand</h2>";
-    userHand.innerHTML="<h2>User Hand</h2>";
-    this.userTotal=0;
-    this.dealerTotal=0;
-    this.userBust=false;
-    this.dealerBust=false;
-    hit.setAttribute("style", "");
-    stand.setAttribute("style", "");
-    dealerScore.setAttribute("style", "");
-    this.curusrHand = new Array();
-    this.curdlrHand = new Array();
+this.emptyDeck = function emptyDeck()
+{
+if(this.deck.length < 1) return true;
+else return false;
+}
 
 
-    for(i=0; i<2; i++)
-    {
-      if(this.emptyDeck())this.newDeck();
-      this.curusrHand.push(this.deck.pop());
-      userHand.innerHTML+=this.curusrHand[i].showCard();
-    }
-    this.userTotal = this.calcValue(this.curusrHand);
-    userScore.innerHTML=this.userTotal;
+this.hit = function hit()
+{
+if(this.emptyDeck())this.newDeck();
+this.currentPlayerHand.push(this.deck.pop());
+userHand.innerHTML+=this.currentPlayerHand[this.currentPlayerHand.length-1].showCard();
+this.userTotal = this.calcValue(this.currentPlayerHand);
+userScore.innerHTML=this.userTotal;
+if(this.userTotal >21)
+{
+userScore.innerHTML+=" <span style='color:red; font-weight: bold;'> BUST</span>";
+this.userBust = true;
+this.gameOver();
+}
+};
 
-    for(i=0; i<2; i++)
-    {
-      if(this.emptyDeck())this.newDeck();
-      this.curdlrHand.push(this.deck.pop());
-      dealerHand.innerHTML+=this.curdlrHand[i].showCard();
-    }
-    this.dealerTotal = this.calcValue(this.curdlrHand);
-    dealerScore.innerHTML=this.dealerTotal;
-    //hide dealers first card
-    var firstCard = dealerHand.getElementsByClassName("card")[0];
-    firstCard.setAttribute("id", "hidden-card");
-    var blackjack =true;
-    if(this.userTotal === 21 && this.dealerTotal < 21) this.gameOver(blackjack);
-    else if(this.dealerTotal === 21) this.gameOver();
-  };
+this.stand = function stand()
+{
+while(this.dealerTotal < 17)
+{
+if(this.emptyDeck())this.newDeck();
+this.houseHand.push(this.deck.pop());
+dealerHand.innerHTML+=this.houseHand[this.houseHand.length-1].showCard();
+this.dealerTotal = this.calcValue(this.curdlrHand);
+dealerScore.innerHTML=this.dealerTotal;
+if(this.dealerTotal > 21)
+{
+dealerScore.innerHTML+=" <span style='color:red; font-weight: bold;'> BUST</span>";
+this.dealerBust = true;
+}
+}
+this.gameOver();
+}
 
-  this.hit = function hit()
-  {
-    if(this.emptyDeck())this.newDeck();
-    this.curusrHand.push(this.deck.pop());
-    userHand.innerHTML+=this.curusrHand[this.curusrHand.length-1].showCard();
-    this.userTotal = this.calcValue(this.curusrHand);
-    userScore.innerHTML=this.userTotal;
-    if(this.userTotal >21)
-    {
-      userScore.innerHTML+=" <span style='color:red; font-weight: bold;'> BUST</span>";
-      this.userBust = true;
-      this.gameOver();
-    }
-  };
+this.gameOver = function gameOver(blackjack)
+{
+document.getElementById("hidden-card").setAttribute("id","");
+dealerScore.setAttribute("style", "visibility: visible;");
+hit.setAttribute("style", "visibility:hidden;");
+stand.setAttribute("style", "visibility:hidden;");
 
-  this.stand = function stand()
-  {
-    while(this.dealerTotal < 17)
-    {
-      if(this.emptyDeck())this.newDeck();
-      this.curdlrHand.push(this.deck.pop());
-      dealerHand.innerHTML+=this.curdlrHand[this.curdlrHand.length-1].showCard();
-      this.dealerTotal = this.calcValue(this.curdlrHand);
-      dealerScore.innerHTML=this.dealerTotal;
-      if(this.dealerTotal > 21)
-      {
-        dealerScore.innerHTML+=" <span style='color:red; font-weight: bold;'> BUST</span>";
-        this.dealerBust = true;
-      }
-    }
-    this.gameOver();
-  }
+if(blackjack)
+{
+this.money +=3;
+status.innerHTML ="BLACKJACK!!!!!!!!!";
+}
 
-  this.gameOver = function gameOver(blackjack)
-  {
-    document.getElementById("hidden-card").setAttribute("id","");
-    dealerScore.setAttribute("style", "visibility: visible;");
-    hit.setAttribute("style", "visibility:hidden;");
-    stand.setAttribute("style", "visibility:hidden;");
+else if(this.userTotal > this.dealerTotal && this.userBust === false || this.dealerBust ===true){
+//user wins
+this.money+=2;
+status.innerHTML ="YOU WIN!";
+}
+else if(this.userTotal === this.dealerTotal && this.userBust === false){
+//push
+this.money++;
+status.innerHTML="PUSH :o";
+}
 
-    if(blackjack)
-    {
-      this.money +=3;
-      status.innerHTML ="BLACKJACK!!!!!!!!!";
-    }
+else status.innerHTML="YOU LOSE!";
+money.innerHTML="Money: "+this.money;
 
-    else if(this.userTotal > this.dealerTotal && this.userBust === false || this.dealerBust ===true){
-      //user wins
-      this.money+=2;
-      status.innerHTML ="YOU WIN!";
-    }
-    else if(this.userTotal === this.dealerTotal && this.userBust === false){
-      //push
-      this.money++;
-      status.innerHTML="PUSH :o";
-    }
+}
 
-    else status.innerHTML="YOU LOSE!";
-    money.innerHTML="Money: "+this.money;
-
-  }
-
-  this.dump = function dump()
-  {
-    for(var i=0; i<this.deck.length; i++)
-    {
-      this.deck[i].showCard();
-    }
-  };
+this.dump = function dump()
+{
+for(var i=0; i<this.deck.length; i++)
+{
+this.deck[i].showCard();
+}
+};
 }
 
 */
