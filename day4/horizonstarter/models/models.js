7ef2967ba3798c5522e62088de38b82e3bf340f8
Tyler <tylerscott.sullivan@outlook.com>
2016-06-17 09:10:05 -0400

    FIxes

---
+++ day4/horizonstarter/models/models.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var connect = require('./connect');
+
+mongoose.connect(connect);
+
+module.exports = {
+	project: mongoose.model('project', {
+		title: {
+			type: String,
+			required: true
+		}
+
+	})
+}
