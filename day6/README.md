01c8bd6a42af148b1deab985a7a168a6dad74a65
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-06-17 13:10:23 -0400

    Add dependency tracker.

---
+++ day6/README.md
@@ -0,0 +1,31 @@
+
+#Dependency resolver
+
+As programmers add more and more features to their applications, codebases get very
+large and unmanageable. That is why we divide it into modules that contain smaller
+blocks of code that is easier to manage. This also increases maintainability and
+encourages reusability. If you develop a module that loads tweets from a certain
+users, you can easily share it so other people can use in the application.
+
+Managing dependencies, though, is a more complex challenge. Certain modules need
+other modules to work correctly. And these modules need other modules and so on,
+creating a graph of dependencies, something like this one. Your task is to get a
+module or component and list all its dependencies in the correct order.
+
+    A
+  ↙  ↘
+  B    C
+↙   ↘ ↗
+E ← D
+
+Here, A depends on B and C. B depends on E and D and so on.
+
+##How does one find the right order of installing the packages?
+
+To find the correct order for installing dependencies, me need to represent the data
+on your program. To do this, we'll use Graphs with modules and edges that symbolize
+dependencies.
+
+## Exercise
+1. Inheritance <br>
+   Files: `week01/day2/depend.js` and `week01/day2/depend.html`
---
+++ day6/depend.js
@@ -0,0 +1,128 @@
+"use strict";
+
+/*
+To start building our dependency manager, we first need a wey to store all of
+our module's dependencies. We need to tell our Tracker (or Dependency manager) which
+module depends on which. There are many ways of doing this, and you can pick the
+one that feels the best. One good option is creating an object "module", that has
+an array for all its dependencies. Another option is to store them by arrays. We
+have decided to store them as key value pairs, where the key is the module and the
+value are its dependencies. For example A depends on B and C; X on Z would be:
+
+{
+  'A': ['B', 'C'],
+  'X': ['Z']
+}
+
+We also need to keep track of all the modules that the user has installed in their
+programs using the install() function. These are the modules the user wants, and
+that can be installed later on.
+
+Finally, we need to have a way to keep track of all the components that our system
+has. That is: The userInstalledComponents + their dependencies.
+*/
+
+function Tracker() {
+  this.dependencies={};
+  this.installedComponents=[];
+  this.userRequiredComponents=[];
+}
+
+// Okay, now we need to start walking through the dependencies graph. We need to
+// get a starting compontent, and then go through all the components it dependes on.
+//For these dependencies we'll have to install their dependencies too! So, we need
+// a recursive function that goes through all the nodes on the graph.
+
+// Write a function that makes 'component' depend on 'anotherComponent'. It should
+// take two strings as arguments and store the dependency relation in the Tracker.
+// A component can depend on many other components, there should be no restrictions
+// on here because components are just declaring their dependencies and not yet
+// being installed.
+Tracker.prototype.depend = function(component, dependsOn) {
+  if(!this.dependencies[component]){
+    this.dependencies[component]=[dependsOn];
+  }else{
+    if(!_.contains(this.dependencies[component], dependsOn)){
+      this.dependencies[component].push(dependsOn)
+    }
+  }
+}
+
+// It should return an array representing all components that need to be installed,
+// in the order they need to be installed to add the dependencies for the module.
+// A good way of going through the dependency tree is using recursion.
+
+// The suggested arguments are (component, resolved, seen), where component is
+// the module that needs to be installed, resolved is an array that is passed to the
+// function to store all the modules that need to be installed, and seen, a blank
+// array to store all the modules that have already been seen by the function (to
+// detect cicrular dependendencies)
+// Possible way of calling this function to install the module 'A':
+// var neededModules = [];
+// getDependenciesForModule('A', neededModules, []);
+// This approach uses recurson to visit other nodes, and thus can't have one single
+// return value, that is why we use the outside var neededModules to call the function.
+Tracker.prototype.getDependenciesForModule = function(component, resolved, seen){
+  seen = seen || [];
+  seen.push(component)
+  if(this.dependencies[component]){
+    for (var i = 0; i<this.dependencies[component].length; i++){
+      var edge=this.dependencies[component][i];
+      if (!_.contains(resolved,edge)){
+        if (_.contains(seen,edge)){
+          throw new Error('Circular dependency Error');
+        }
+        this.getDependenciesForModule(edge, resolved, seen);
+      }
+    }
+  }
+  resolved.push(component)
+}
+
+// Write a function that takes a component and installs it. It should save this
+// module to the Tracker as a module the user has requested.
+// It also needs to add all the components to our system, meaning the installed
+// module and all its dependencies using getDependenciesForModule();
+//
+// Bonus: Adding circular dependencies like A depends on B and B depends on A cannot
+// be possible. Throw an exception when installing circular deps.
+Tracker.prototype.install = function(component) {
+  var componentsNeeded=[];
+  this.userRequiredComponents = _.union(this.userRequiredComponents,[component]);
+  this.getDependenciesForModule(component, componentsNeeded);
+  this.installedComponents=_.union(this.installedComponents, componentsNeeded);
+}
+
+// Write a function that uninstalls the given component. The function should receive
+// one string with the module that must be uninstalled.  If it has not been installed,
+// throw an exception. The module has to be removed from the userInstalledComponents.
+
+// Here comes the tricky part:
+// If there were modules that the uninstalled component needed, and no other component
+// needs them, they should be uninstalled too.
+// Return an array of components that were uninstalled, in the order they
+// were uninstalled.
+
+Tracker.prototype.uninstall = function(component) {
+  var uninstalled = this.installedComponents.slice();
+  if(_.contains(this.userRequiredComponents, component)){
+    this.userRequiredComponents = _.difference(this.userRequiredComponents , [component]);
+    this.installedComponents=[];
+    for (var i = 0; i<this.userRequiredComponents.length; i++){
+      this.install(this.userRequiredComponents[i])
+    }
+    return _.difference(uninstalled, this.installedComponents);
+  }else{
+    throw new Error('Component not installed Error');
+  }
+}
+
+// Get an array representing all components that have been installed by the user
+Tracker.prototype.getUserInstalled = function() {
+  return this.userRequiredComponents;
+}
+// Get an array representing all components that have been installed by the user
+// plus their dependencies
+Tracker.prototype.getAllInstalledComponents = function() {
+  return this.installedComponents;
+}
