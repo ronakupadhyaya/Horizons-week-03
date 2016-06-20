e8009d964e1f8f93d2ed5fa17103813f70a070fb
aschugart <annieschugart@Annies-MacBook-Pro-2.local>
2016-06-20 09:05:18 -0400

    saturday things

--- day6/keyvaluestore.js
+++ day6/keyvaluestore.js
@@ -32,8 +32,10 @@
 // Add a 'length' property to the KeyValueStore with a value of 0,
 // And add a 'pairs' property and give it a blank object as value
 function KeyValueStore() {
-  // YOUR CODE HERE
+  this.length = 0;
+  this.pairs = {};
 }
+
 
 // Write a function that adds new (key,value) pairs to the store.
 // If the the store is empty -> kvs.pairs = {}
@@ -45,34 +47,44 @@ function KeyValueStore() {
 // kvs.put('Other key', 'Other Value')
 // Then the store kvs.pairs should have -> { '53': 3, McDonalds: 12, Subway: 2, 'Other key': 'Other Value' }
 KeyValueStore.prototype.put = function(key, value) {
-  // YOUR CODE HERE
+  this.length++;
+  this.pairs[key] = value;
 }
 
 // Write a function that checks if the store contains a specific key;
 // It should receive a key of any type: object, number, string
 // And return true or false if the object has the key.
 KeyValueStore.prototype.containsKey = function(key) {
-  // YOUR CODE HERE
+  return this.pairs.hasOwnProperty(key);
 }
 
 // Write a function that gets the values for a given key.
 // It should receive the value of key as an argument
 // And return either the key, or undefined if it is not on the store.
 KeyValueStore.prototype.get = function(key) {
-  // YOUR CODE HERE
+  if (this.containsKey(key)){
+    return this.pairs[key]
+  } else {
+    return undefined;
+  }
 }
+
 
 // Write a function that returns how many items we have in store.
 // It shouldn't take arguments, and it should return a number.
 KeyValueStore.prototype.getSize = function() {
-  // YOUR CODE HERE
+  return this.length;
 }
 
 // This function is very similar to containsKey(key), but this time the other way
 // around. It should receive a value, and search for the key that is associated with
 // that value and return it.
 KeyValueStore.prototype.containsValue = function(value) {
-  // YOUR CODE HERE
+  for (var i; i < this.length; i ++) {
+    if (this.get(key) === value) {
+      return value;
+    }
+  }
 }
 
 // Implement a function that deletes a certain (key,value) pair.
@@ -102,4 +114,4 @@ KeyValueStore.prototype.values = function(fn) {
 // first item is the key and the second item is the value: fn([key, value])
 KeyValueStore.prototype.keysValues = function(fn) {
   // YOUR CODE HERE
-}
+};
