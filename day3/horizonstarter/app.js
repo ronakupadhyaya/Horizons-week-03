5f8a6a30d2c53db285d35fdad7073ac822c493ea
Moose Paksoy <mustafa@joinhorizons.com>
2017-02-01 01:52:33 -0800

    Project structure!

--- day3/horizonstarter/app.js
+++ day3/horizonstarter/app.js
@@ -1,41 +1,44 @@
 "use strict";
 
+// Express setup
+var fs = require('fs');
 var express = require('express');
-var session = require('express-session');
+var exphbs  = require('express-handlebars');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
-var routes = require('./routes/index');
 
+// Initialize Express
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// mongoose configuration
+var mongoose = require('mongoose');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+if (! fs.existsSync('./config.js')) {
+  throw new Error('config.js is missing');
+}
+var config = require('./config');
+if (! config.mongoUrl) {
+  throw new Error('mongoUrl is missing in config.js');
+}
+mongoose.connect(config.mongoUrl);
+
+// Handlabars setup
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(validator());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Session allows us to store data in a session object between HTTP requests.
-// It allows us to pass "flash" data between requests, so that we can redirect
-// the user and they'll still see the flash message.
-// Request requires a "secret" to encrypt the data. It relies on cookies but
-// does it all transparently.
-app.use(session({secret: 'cutie kitty catz'}));
-
-// All of our routes are configured here. (For a more complex app, we could
-// split our routes up into multiple route files.)
+// All of our routes are in routes.js
+var routes = require('./routes');
 app.use('/', routes);
 
+// error handlers
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -43,7 +46,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
 // development error handler
 // will print stacktrace
@@ -67,9 +69,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-// mongo configuration
-var dbConfig = require('./config/db.js');
-var mongoose = require('mongoose');
-mongoose.connect(dbConfig.url);
 
-module.exports = app;
+app.listen(process.env.PORT || 3000);
