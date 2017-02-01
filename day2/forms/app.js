ede80155cc9ebc88a501c0b76b5a55f7b18db810
Snuupy <snuupy[-removethis-]@snusite.com>
2017-01-31 20:06:39 -0800

    user profiles

--- day2/forms/app.js
+++ day2/forms/app.js
@@ -8,7 +8,9 @@ var path = require('path');
 // Set up handlebar templates
 var exphbs = require('express-handlebars');
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.engine('.hbs', exphbs({
+  extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 // Enable form validation with express validator.
@@ -17,14 +19,16 @@ app.use(expressValidator());
 
 // Enable POST request body parsing
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTES
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.redirect('/register');
 });
 
@@ -32,9 +36,8 @@ app.get('/', function(req, res){
 // This is the endpoint that the user loads to register.
 // It contains an HTML form that should be posted back to
 // the server.
-app.get('/register', function(req, res){
+app.get('/register', function(req, res) {
   // YOUR CODE HERE
-  console.log(req);
   res.render('register');
 });
 
@@ -42,23 +45,63 @@ app.get('/register', function(req, res){
 // Write a function that takes a request object and does
 // validation on it using express-validator.
 function validate(req) {
-  req.checkBody('firstName', 'Invalid firstName').notEmpty();
+  var initial = {
+    'mInitial': {
+      isLength: {
+        options: [{min: 0, max: 1}],
+        errorMessage: "Invalid initial"
+      }
+    }
+  };
+
+  app.use(expressValidator({
+    customValidators: {
+      gte: function(param, num){
+        return param>=num;
+      },
+      lte: function(param, num){
+        return param<=num;
+      },
+      same: function(param, str){
+        return param === str;
+      }
+    }
+  }))
+
+  req.checkBody('firstName', 'Invalid first name').notEmpty();
+  req.checkBody(initial);
+  req.checkBody('lastName', 'Invalid last name').notEmpty();
+  req.checkBody('month', 'Invalid month').gte(1).lte(12);
+  req.checkBody('day', 'Invalid day').gte(1).lte(31);
+  req.checkBody('year', 'Invalid year').gte(0);
+  req.checkBody('password', 'Invalid password').notEmpty();
+  req.checkBody('password2', 'Invalid password2').notEmpty().same(req.body.password);
+  // req.checkBody('optradio', 'Invalid gender').notEmpty();
+  req.checkBody('newsletter', 'Invalid newsletter').notEmpty();
+  req.checkBody('bio', 'Invalid bio').notEmpty();
+  // req.checkBody('date', 'Invalid date').not.notEmpty();
+  console.log(req.body.password);
 }
 
 // ---Part 3: Render errors and profile---
 // POST /register
 // This is the endpoint that the user hits when they submit
 // the registration form.
-app.post('/register', function(req, res){
+app.post('/register', function(req, res) {
   validate(req);
+  console.log(req.body);
   // Get errors from express-validator
   var errors = req.validationErrors();
   if (errors) {
-    res.render('register', {errors: errors});
+    console.log(errors);
+    res.render('register', {
+      errors: errors
+    });
   } else {
     // YOUR CODE HERE
     // Include the data of the profile to be rendered with this template
-    res.render('profile');
+    req.body.date = new Date();
+    res.render('profile', req.body);
   }
 });
 
