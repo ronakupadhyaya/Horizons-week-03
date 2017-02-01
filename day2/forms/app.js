06023503399971d1a3aaf61b2ec151484ecfea27
gsmith98 <smitgw11@gmail.com>
2017-01-31 21:42:08 -0800

    actually pushing ex 1 this time

--- day2/forms/app.js
+++ day2/forms/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var _ = require('underscore');
 
 // Set up handlebar templates
 var exphbs = require('express-handlebars');
@@ -13,7 +14,15 @@ app.set('view engine', '.hbs');
 
 // Enable form validation with express validator.
 var expressValidator = require('express-validator');
-app.use(expressValidator());
+app.use(expressValidator({
+  customValidators: {
+    singleCharacter: function(value) {return value.length === 1},
+    inRange: function(value, lowbound, highbound) {return value >= lowbound && value <= highbound},
+    greaterThan: function(value, lowbound) {return value >= lowbound},
+    isEqual: function(value, secondval) {return value === secondval},
+    oneOf: function(value, arr) {return arr.includes(value)}
+  }
+}));
 
 // Enable POST request body parsing
 var bodyParser = require('body-parser');
@@ -33,15 +42,29 @@ app.get('/', function(req, res){
 // It contains an HTML form that should be posted back to
 // the server.
 app.get('/register', function(req, res){
-  // YOUR CODE HERE
-  res.render('register');
+  res.render('register', {
+    header: "here it is",
+    days: _.range(1,32),
+    months: _.range(1,13),
+    currentDate: new Date()
+});
 });
 
 // ---Part 2: Validation---
 // Write a function that takes a request object and does
 // validation on it using express-validator.
 function validate(req) {
-  req.checkBody('firstName', 'Invalid firstName').notEmpty();
+  console.log(req)
+  req.checkBody('firstName', 'Invalid first name').notEmpty();
+  if (req.body.middleInitial) req.checkBody('middleInitial', 'Middle initial must be one letter').singleCharacter();
+  req.checkBody('lastName', 'Invalid last name').notEmpty();
+  if (req.body.dobMonth) req.checkBody('dobMonth', 'Invalid birth month').isInt().inRange(1,12);
+  if (req.body.dobDay)   req.checkBody('dobDay', 'Invalid birth day').isInt().inRange(1,31);
+  if (req.body.dobYear)  req.checkBody('dobYear', 'Invalid birth year').isInt().greaterThan(1900);
+  req.checkBody('password', 'Invalid password').notEmpty().isEqual(req.body.repeatPassword);
+  req.checkBody('genderRadios', 'Invalid gender').notEmpty().oneOf(['male', 'female', 'helicopter']);
+  req.checkBody('subCheckbox', 'Our newsletter is really good! Sign up!').notEmpty();
+  req.checkBody('bio', 'Invalid bio').notEmpty();
 }
 
 // ---Part 3: Render errors and profile---
@@ -52,10 +75,20 @@ app.post('/register', function(req, res){
   validate(req);
   // Get errors from express-validator
   var errors = req.validationErrors();
+  console.log(errors)
   if (errors) {
-    res.render('register', {errors: errors});
+    res.render('register', {
+      errors: errors,
+      header: "here it is",
+      days: _.range(1,32),
+      months: _.range(1,13),
+      currentDate: new Date()
+  }
+
+  );
   } else {
     // YOUR CODE HERE
+
     // Include the data of the profile to be rendered with this template
     res.render('profile');
   }
