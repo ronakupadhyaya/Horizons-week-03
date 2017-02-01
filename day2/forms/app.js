9cdd45173e7d4d3e07d73b25e1464b2db9a14795
dnajafi <dbnajafi@gmail.com>
2017-01-31 20:09:58 -0800

    [finished] into to HTML forms

--- day2/forms/app.js
+++ day2/forms/app.js
@@ -32,16 +32,48 @@ app.get('/', function(req, res){
 // This is the endpoint that the user loads to register.
 // It contains an HTML form that should be posted back to
 // the server.
-app.get('/register', function(req, res){
-  // YOUR CODE HERE
-  res.render('register');
+app.get('/register', function(request, response){
+  response.render('register');
 });
+
+app.use(expressValidator({
+ customValidators: {
+    gte: function(param, num) {
+        return param >= num;
+    },
+    isOneLetter: function(param){
+
+      return param.length === 1;
+
+    },
+    isBetween: function(param, first, last){
+
+      if(param <= last && param >= first) return true;
+
+      return false;
+    },
+    equalTo: function(param1, param2){
+      return param1 === param2;
+    }
+
+ }
+}));
+
 
 // ---Part 2: Validation---
 // Write a function that takes a request object and does
 // validation on it using express-validator.
 function validate(req) {
   req.checkBody('firstName', 'Invalid firstName').notEmpty();
+  req.checkBody('middleName', 'Invalid Middle Name').notEmpty().isOneLetter();
+  req.checkBody('lastName', 'Invalid Last Name').notEmpty();
+  req.checkBody('month', 'Month must be between 1 and 12').isBetween(1, 12);
+  req.checkBody('day', 'Day must be between 1 and 32').isBetween(1, 31);
+  req.checkBody('year', 'Year must be nonnegative.');
+  req.checkBody('password', 'Password field must not be empty').notEmpty();
+  req.checkBody('repeatPassword', 'Passwords must match').notEmpty().equalTo(req.body.password);
+  req.checkBody('biography', 'Biography field must not be empty').notEmpty();
+
 }
 
 // ---Part 3: Render errors and profile---
@@ -50,6 +82,7 @@ function validate(req) {
 // the registration form.
 app.post('/register', function(req, res){
   validate(req);
+  console.log(req.body);
   // Get errors from express-validator
   var errors = req.validationErrors();
   if (errors) {
@@ -57,7 +90,7 @@ app.post('/register', function(req, res){
   } else {
     // YOUR CODE HERE
     // Include the data of the profile to be rendered with this template
-    res.render('profile');
+    res.render('profile', req.body);
   }
 });
 
