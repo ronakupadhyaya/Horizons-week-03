d81f36b25a45be0e90022b0b1da4ee11c048bd06
Moose Paksoy <mustafa@joinhorizons.com>
2017-02-02 07:50:22 -0800

    Remove solution.

--- day4/time-machine/README.md
+++ day4/time-machine/README.md
@@ -6,14 +6,21 @@ In this exercise we're going to build a simple form that allows you to
 add or subtract days, months or years from a given date
 
 1. Open `app.js` and `views/index.hbs` in your editor.
-1. Make the `<input type="date">`
-  1. Pass
-  1. Increase `count` by one
-  1. Redirect to `/`
-1. Run your server and click on the `Increase` button, the count should go up.
-1. Implement the `POST /decrement` endpoint (aka route). This route should:
-  1. Decrease `count` by one
-  1. Redirect to `/`
-1. Add a form to `views/index.hbs` that contains a single button that
-  1. `POST`s to `/decrement` when clicked
-  1. Has the label `Decrease`
+1. Make the `<input type="date">` value change based on the `when` query parameter
+1. Make the `<input type="number">` value change based on the `amount` query parameter
+1. Make the `<select>` value change based on the `units` query parameter
+
+  HTML select elements don't have a `value` attribute. You have to set the
+  `selected` attribute on one of the `<option>`s. You may find Handlebars
+  helper useful for this purpose.
+
+1. When the `To the past` button is clicked make the `<input type="date">`
+   value move `amount` `units` to the future.
+1. When the `To the future!` button is clicked make the `<input type="date">`
+   value move `amount` `units` to the past.
+
+There's a `toDateStr()` function that converts JavaScript `Date` objects 
+into date-time strings expected by `<input type="date">` elements. You may find
+it useful 😉!
+
+### [Try working version here](https://time-machine.gomix.me/)
--- day4/time-machine/app.js
+++ day4/time-machine/app.js
@@ -8,14 +8,8 @@ app.engine('.hbs', exphbs(
   {
     extname: '.hbs',
     helpers: {
+      // You can define an Handlebars helper here
       // YOUR CODE HERE
-      select: function(selectedOption) {
-        var options = ['days', 'months', 'years'];
-        return options.map(function(option) {
-          var selected = selectedOption === option ? 'selected' : '';
-          return `<option ${selected}>${option}</option>`
-        }).join('');
-      }
     }
   }));
 app.set('view engine', '.hbs');
@@ -25,35 +19,14 @@ function pad(num) {
   return (norm < 10 ? '0' : '') + norm;
 }
 
+// This function
 function toDateStr(date) {
-  return date.getFullYear() + '-' + pad(date.getMonth() + 1) + '-' + pad(date.getDate() + 1);
+  return date.getFullYear() + '-' + pad(date.getMonth() + 1) + '-' + pad(date.getDate());
 }
 
 app.get('/', function(req, res) {
   // YOUR CODE HERE
-  var when = new Date(req.query.when);
-  var amount = parseInt(req.query.amount) || 0;
-
-  var combine;
-  if (req.query.action === 'to the future!') {
-    combine = function(a, b) { return a + b; }
-  } else {
-    combine = function(a, b) { return a - b; }
-  }
-
-  if (req.query.units === 'days') {
-    when.setDate(combine(when.getDate(), amount));
-  } else if (req.query.units === 'months') {
-    when.setMonth(combine(when.getMonth(), amount));
-  } else if (req.query.units === 'years') {
-    when.setFullYear(combine(when.getFullYear(), amount));
-  }
-  console.log(when);
-  res.render('index', {
-    when: toDateStr(when),
-    amount: amount,
-    units: req.query.units
-  });
+  res.render('index');
 });
 
 app.listen(3000);
