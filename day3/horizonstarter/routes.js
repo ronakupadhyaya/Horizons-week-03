904f90c98c9705c4bce60758d5ff1cf57c063a2b
derekelec <mathewson17@live.ca>
2017-02-02 09:18:56 -0800

    week3day3

--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -6,6 +6,7 @@ var router = express.Router();
 var Project = require('./models').Project;
 var strftime = require('strftime');
 
+
 // Example endpoint
 router.get('/create-test-project', function(req, res) {
   var project = new Project({
@@ -22,26 +23,53 @@ router.get('/create-test-project', function(req, res) {
 
 // Exercise 1: View all projects
 // Implement the GET / endpoint.
-router.get('/', function(req, res) {
-  // YOUR CODE HERE
+router.get('/', function(req, res) { //local
+  Project.find(function(err,project){
+    if(err){
+      throw err
+    }else{
+      res.render('index.hbs',{name: project});
+      }
+  })
 });
 
 // Exercise 2: Create project
 // Implement the GET /new endpoint
 router.get('/new', function(req, res) {
-  // YOUR CODE HERE
+ res.render('new.hbs');
 });
 
 // Exercise 2: Create project
 // Implement the POST /new endpoint
 router.post('/new', function(req, res) {
-  // YOUR CODE HERE
+  req.checkBody('goal')
+  var newProj = req.body;
+  var sendProject = new Project({
+    title: newProj.title,
+    goal: parseInt(newProj.goal),
+    description: newProj.description,
+    start: new Date(newProj.start),
+    end: new Date(newProj.end)
+  })
+  sendProject.save(function(err) {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.redirect('/')
+    }
+  });
+
 });
 
 // Exercise 3: View single project
 // Implement the GET /project/:projectid endpoint
 router.get('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  Project.findById(req.params.projectid,function(err,item){
+    if(err) throw err;
+    else {
+      res.render('project.hbs',{project:item})
+    }
+  })
 });
 
 // Exercise 4: Contribute to a project
