c8942b73ab8c5df068edf369e5a8a9b948c23ec9
gsmith98 <smitgw11@gmail.com>
2017-02-02 12:33:45 -0800

    finished up to exercise6 on horizonstarter

--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -23,35 +23,106 @@ router.get('/create-test-project', function(req, res) {
 // Exercise 1: View all projects
 // Implement the GET / endpoint.
 router.get('/', function(req, res) {
-  // YOUR CODE HERE
+  Project.find(function(error, array) {
+    if (error) {
+      console.log("Error: ", error);
+    } else {
+      res.render('index', {items: array});
+    }
+  });
 });
 
 // Exercise 2: Create project
 // Implement the GET /new endpoint
 router.get('/new', function(req, res) {
-  // YOUR CODE HERE
+  res.render('new')
 });
 
 // Exercise 2: Create project
 // Implement the POST /new endpoint
 router.post('/new', function(req, res) {
-  // YOUR CODE HERE
+  req.checkBody('title', 'Invalid title').notEmpty();
+  req.checkBody('goal', 'Invalid goal').isInt();
+  req.checkBody('description', 'Invalid description').notEmpty();
+  req.checkBody('start', 'Invalid start date').notEmpty();
+  req.checkBody('end', 'Invalid end date').notEmpty();
+  var errors = req.validationErrors();
+  if (errors) {
+    req.body.errors = errors;
+    res.render('new', req.body)
+  } else {
+    req.body.contributions = [];
+    var newProject = new Project(req.body);
+    newProject.save(function(err) {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  }
 });
 
 // Exercise 3: View single project
 // Implement the GET /project/:projectid endpoint
 router.get('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  Project.findById(req.params.projectid, function(error, proj) {
+    if (error) {
+      res.status(500).json(error);
+    } else {
+      res.render('project', proj)
+    }
+  })
 });
 
 // Exercise 4: Contribute to a project
 // Implement the GET /project/:projectid endpoint
 router.post('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  console.log(req)
+  Project.findById(req.params.projectid, function(error, proj) {
+    if (error) {
+      res.status(500).json(error);
+    } else {
+      // console.log("THERE",req.body);
+      // console.log(req.body.amount);
+      proj.contributions.push({name: req.body.name, amount:req.body.amount});
+      console.log("HERE", proj);
+      proj.save(function(err) {
+        if (err) {
+          res.status(500).json(err);
+        } else {
+          console.log(proj)
+          res.redirect("/project/" + req.params.projectid);
+        }
+      })
+    }
+  })
 });
 
 // Exercise 6: Edit project
 // Create the GET /project/:projectid/edit endpoint
+router.get('/project/:projectid/edit', function(req, res) {
+  Project.findById(req.params.projectid, function(error, proj) {
+    if (error) {
+      res.status(500).json(error);
+    } else {
+      console.log(proj)
+      res.render('editProject', proj);
+    }
+  })
+});
+
 // Create the POST /project/:projectid/edit endpoint
+router.post('/project/:projectid/edit', function(req, res) {
+  console.log(req.body);
+  Project.findByIdAndUpdate(req.params.projectid, {$set: req.body}, function(error, proj) {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        console.log(proj)
+        res.redirect("/project/" + req.params.projectid);
+      }
+  });
+});
 
 module.exports = router;
