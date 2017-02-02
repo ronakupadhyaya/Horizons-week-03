5c1f43e1e1f0537e3a38d54d6026b7f7b9974854
lisaztruong <lisaztruong@gmail.com>
2017-02-02 15:32:46 -0800

    what have I done

--- day4/time-machine/app.js
+++ day4/time-machine/app.js
@@ -10,7 +10,9 @@ app.engine('.hbs', exphbs(
     helpers: {
       // You can define an Handlebars helper here
       // YOUR CODE HERE
+      datetime: when.toDateStr();
     }
+    name: param.query.amount;
   }));
 app.set('view engine', '.hbs');
 
@@ -21,12 +23,39 @@ function pad(num) {
 
 // This function
 function toDateStr(date) {
-  return date.getFullYear() + '-' + pad(date.getMonth() + 1) + '-' + pad(date.getDate());
+  return date.getFullYear() + '-' + pad(date.getMonth() + 1) + '-' + pad(date.getDate()+1);
 }
 
 app.get('/', function(req, res) {
   // YOUR CODE HERE
-  res.render('index');
+  var when = req.query.when;
+  // parse the date
+  var date = new Date(when);
+
+  // prevent copied CODE
+  var amount = parseInt(req.query.amount);
+
+  if (req.query.action === 'to the future!'){
+    amount = amount;
+  }else{
+    amount = -amount;
+  }
+
+  if (req.query.units === 'days'){
+    // parse int because we get stuff as str from browser
+    date.setDate(date.getDate() + amount);
+  } else if (req.query.units ==== 'months'){
+    date.setMonth(date.getMonth() + amount)
+  } else if (req.query.units ==== 'years'){
+    date.setFullYear(date.getFullYear() + amount)
+  }
+
+  res.render('index',{
+    // turn time back to string
+    time: toDateStr(date),
+    howMuch: req.query.amount
+
+  });
 });
 
 app.listen(3000);
