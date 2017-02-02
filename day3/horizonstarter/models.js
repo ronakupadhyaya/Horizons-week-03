1ddf39dc9294f4a10bb5f5b8319e9bdd6a19d1b9
Holly <hrli@wharton.upenn.edu>
2017-02-01 20:31:07 -0800

    DONE

--- day3/horizonstarter/models.js
+++ day3/horizonstarter/models.js
@@ -6,7 +6,31 @@ var mongoose = require('mongoose');
 var Project = mongoose.model('Project', {
   title: {
     type: String,
-  }
+    required: true
+  },
+  goal: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  start: {
+    type: Date,
+    required: true
+  },
+  end: {
+    type: Date,
+    required: true
+  },
+  contributions: [
+    {
+      name: String,
+      amount: Number
+    }
+  ]
+
+  
   // YOUR CODE HERE
 });
 
--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -23,32 +23,125 @@ router.get('/create-test-project', function(req, res) {
 // Exercise 1: View all projects
 // Implement the GET / endpoint.
 router.get('/', function(req, res) {
-  // YOUR CODE HERE
+  Project.find(function(err, arr){ //second thing is result
+    if(err){
+      console.log("there's an error");
+    }
+    console.log("success");
+    res.render('index', {items: arr}); //should be callback object
+
+  })
 });
 
 // Exercise 2: Create project
 // Implement the GET /new endpoint
 router.get('/new', function(req, res) {
-  // YOUR CODE HERE
+  res.render('new');
 });
 
 // Exercise 2: Create project
 // Implement the POST /new endpoint
+
+function validate(req){
+  req.checkBody('title', 'Invalid').notEmpty();
+  req.checkBody('goal', 'Invalid').notEmpty();
+  req.checkBody('description', 'Invalid').notEmpty();
+  req.checkBody('start', 'Invalid').notEmpty();
+  req.checkBody('end', 'Invalid').notEmpty();
+
+}
+
 router.post('/new', function(req, res) {
+
   // YOUR CODE HERE
+  validate(req);
+  var errors = req.validationErrors();
+  if (errors) {
+    res.render('new', {errors: errors});
+  } else {
+    var newProject = new Project({
+      title: req.body.title,
+      goal: req.body.goal,
+      description: req.body.description,
+      start: req.body.start,
+      end: req.body.end
+    });
+    newProject.save(function(err){
+      console.log("save")
+      if (err){
+        console.log(err)
+      }
+      res.redirect('/');
+    //  res.render('/', {data: newProject})
+    })
+
+
+    // res.render('new');
+  }
 });
 
 // Exercise 3: View single project
 // Implement the GET /project/:projectid endpoint
 router.get('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  // console.log("whee");
+  Project.findById(req.params.projectid, function(err, project){
+    console.log(req.params.projectid);
+    console.log('123');
+    if(err){
+      console.log("error finding project", err)
+    } else {
+      console.log(project);
+      res.render('project', {project:project}) //the key here could be anything, and should be used in the handlebars in project
+    }
+  })
 });
 
 // Exercise 4: Contribute to a project
 // Implement the GET /project/:projectid endpoint
 router.post('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
-});
+  Project.findById(req.params.projectid, function(err, project){
+    if(err){
+      console.log(this[project][contribution]);
+      console.log("error finding project", err)
+    } else {
+
+      // var newContribution = req.body;
+      var newContribution = {
+        name: req.body.name,
+        amount: req.body.contributions
+      }
+      project.contributions.push(newContribution);
+      project.save(function(err){
+        if (err){
+          console.log(err)
+        }
+
+        var total = 0;
+        project.contributions.forEach(function(item){
+        total += item.amount;
+      });
+
+        var percent = total/project.goal*100
+        if(percent> 100){
+        percent = 100;
+        }
+
+
+        // var total = 0;
+        // project.contributions.forEach(function(item){
+        //   total += item.amount;
+        // });
+        //
+        // var percent = total/project.goal * 100;
+        // if(percent > 100){
+        //   percent = 100;
+        // }
+        res.render('project', {project:project, total:total, percent:percent})
+        //res.redirect('project', {project:project});
+      })
+      }
+    })
+  })
 
 // Exercise 6: Edit project
 // Create the GET /project/:projectid/edit endpoint
