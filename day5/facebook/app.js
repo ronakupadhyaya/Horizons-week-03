7c3fb9bc8b4a2414674f8d0358cd3e39264e2e90
HaMinKo <hko1@babson.edu>
2017-02-03 20:46:50 -0800

    finished boi

--- day5/facebook/app.js
+++ day5/facebook/app.js
@@ -79,7 +79,7 @@ app.post('/api/users/login', function(req, res) {
       res.status(400).json(err)
     };
     if (req.body.email === '' || req.body.password === '') {
-      res.status(301).json("Missing information");
+      res.status(301).json({"Error": "Missing information"});
     } else {
 
       var login = false;
@@ -91,7 +91,7 @@ app.post('/api/users/login', function(req, res) {
       };
 
       if (login === false) {
-        res.status(301).json("Email or password doesn't match")
+        res.status(301).json({"Error": "Email or password doesn't match"})
       } else {
         var newUserToken = new Token({
             userId: req.body.email,
@@ -118,53 +118,71 @@ app.post('/api/users/login', function(req, res) {
 
 app.get('/api/users/login', function(req, res) {
   res.status(401).json({
-    "error": "Login failed."
+    "Error": "Login failed."
   });
 });
 
 app.get('/api/users/error', function(req, res) {
   res.status(401).json({
-    "error": "Action not allowed. Please authenticate."
+    "Error": "Action not allowed. Please authenticate."
   });
 });
 
-app.get('/api/posts', function(req, res) {
-  Post.find(function(err, posts) {
+app.get('/api/posts/:token', function(req, res) {
+  Token.findOne({'token': req.params.token}, function(err, token){
     if (err) {
-      res.status(400).json(err)
+      res.status(400).json(err);
     };
-    posts.sort(function(a, b) {
-      return new Date(a.createdAt) - new Date(b.createdAt)
-    });
-    var postSlice = posts.slice(0, 9)
-    res.status(200).json({
-      "success": true,
-      "response": postSlice
-    })
+    if (token === null) {
+      res.status(400).json({"Error": "Token not found."});
+    } else {
+      Post.find(function(err, posts) {
+        if (err) {
+          res.status(400).json(err)
+        };
+        posts.sort(function(a, b) {
+          return new Date(a.createdAt) - new Date(b.createdAt)
+        });
+        var postSlice = posts.slice(0, 9)
+        res.status(200).json({
+          "success": true,
+          "response": postSlice
+        })
+      });
+    }
   });
 });
 
-app.get('/api/posts/:numberOfPosts', function(req, res) {
-  Post.find(function(err, posts) {
+app.get('/api/posts/:numberOfPosts/:token', function(req, res) {
+  Token.findOne({'token': req.params.token}, function(err, token){
     if (err) {
-      res.status(400).json(err)
+      res.status(400).json(err);
     };
-    posts.sort(function(a, b) {
-      return new Date(a.createdAt) - new Date(b.createdAt)
-    });
-    var postSlice;
-    var postSlicer = function(n) {
-      if (posts.length > (n*10 - 10)) {
-        return postSlice = posts.slice(0, (n*10 - 1));
-      } else  {
-        return postSlice = [];
-      }
-    };
-    postSlicer(req.params.numberOfPosts);
-    res.status(200).json({
-      "success": true,
-      "response": postSlice
-    })
+    if (token === null) {
+      res.status(400).json({"Error": "Token not found."});
+    } else {
+      Post.find(function(err, posts) {
+        if (err) {
+          res.status(400).json(err)
+        };
+        posts.sort(function(a, b) {
+          return new Date(a.createdAt) - new Date(b.createdAt)
+        });
+        var postSlice;
+        var postSlicer = function(n) {
+          if (posts.length > (n*10 - 10)) {
+            return postSlice = posts.slice(0, (n*10 - 1));
+          } else  {
+            return postSlice = [];
+          }
+        };
+        postSlicer(req.params.numberOfPosts);
+        res.status(200).json({
+          "success": true,
+          "response": postSlice
+        })
+      });
+    }
   });
 });
 
@@ -174,9 +192,9 @@ app.post('/api/posts', function(req, res) {
       res.status(400).json(err)
     };
     if (token === null) {
-      res.status(400).json("Token not found")
+      res.status(400).json({"Error": "Token not found."})
     } else if (req.body.content === '') {
-      res.status(400).json({'error': "No post content."});
+      res.status(400).json({'Error': "No post content."});
     } else {
       User.findOne({'email': token.userId}, function(err, user){
         var newPost = new Post({
@@ -210,17 +228,16 @@ app.post('/api/posts/comments/:id', function (req, res) {
       res.status(400).json(err)
     }
     if (token === null) {
-      res.status(400).json("token not found")
+      res.status(400).json({"Error": "Token not found"})
     } else if (req.body.content === '') {
-      res.status(400).json({"error": "Comment field empty"})
+      res.status(400).json({"Error": "Comment field empty"})
     } else {
-      Post.findOne({'id': req.params.id}, function(err, post) {
-        console.log(post)
+      Post.findOne({'_id': req.params.id}, function(err, post) {
         if (err) {
           res.status(400).json(err)
         }
         if (post === null) {
-          res.status(400).json("post not found");
+          res.status(400).json({"Error": "Post not found"});
         } else {
           User.findOne({'email': token.userId}, function(err, user) {
             var newComment = {
@@ -233,14 +250,21 @@ app.post('/api/posts/comments/:id', function (req, res) {
             };
             var newCommentArray = post.comments
             newCommentArray.push(newComment)
-            Post.findOneAndUpdate({'id': req.params.id}, {"comments": newCommentArray}, function(err, postUpdate) {
+            Post.findOneAndUpdate({'_id': req.params.id}, {"comments": newCommentArray}, function(err, postUpdate) {
               if (err) {
-                res.status(400).json("Couldn't post comment")
+                res.status(400).json({"Error": "Couldn't post comment"})
               } else {
-                res.status(200).json({
-                  "success": true,
-                  "response": postUpdate
-                })
+                Post.findOne({'_id': req.params.id}, function(err, postUpdateShow) {
+                  if (err) {
+                    res.status(400).json({"Error": "Couldn't find updated post"})
+                  } else {
+                    console.log(postUpdateShow)
+                    res.status(200).json({
+                      "success": true,
+                      "response": postUpdateShow
+                    });
+                  }
+                });
               }
             });
           });
@@ -250,6 +274,80 @@ app.post('/api/posts/comments/:id', function (req, res) {
   });
 });
 
+app.get('/api/posts/likes/:id/:token', function(req, res) {
+  Token.findOne({'token': req.params.token}, function(err, token) {
+    if (err) {
+      res.status(400).json(err);
+    };
+    if (token === null) {
+      res.status(400).json({"Error": "Token not found."});
+    } else {
+      Post.findOne({'_id': req.params.id}, function(err, post) {
+        if (err) {
+          res.status(400).json(err);
+        };
+        if (post === null) {
+          res.status(400).json({"Error": "Post not found."});
+        } else {
+          User.findOne({'email': token.userId}, function(err, user) {
+            if (err) {
+              res.status(400).json(err);
+            };
+            var likeUser = {
+              "name": user.fname + ' ' + user.lname,
+              "id": user.id
+            }
+            var newLikeArray = post.likes
+            if (newLikeArray.filter(function(a){return a.id === user.id}).length === 0) {
+              newLikeArray.push(likeUser)
+            } else {
+              var removeIndex = newLikeArray.map(function(item) { return item.id; }).indexOf(user.id);
+              newLikeArray.splice(removeIndex, 1);
+            }
+            Post.findOneAndUpdate({'_id': req.params.id}, {'likes': newLikeArray}, function(err, postUpdate){
+              if (err) {
+                res.status(400).json(err);
+              } else {
+                Post.findOne({'_id': req.params.id}, function(err, postUpdateShow) {
+                  if (err) {
+                    res.status(400).json({"Error": "Couldn't find updated post"})
+                  } else {
+                    console.log(postUpdateShow)
+                    res.status(200).json({
+                      "success": true,
+                      "response": postUpdateShow
+                    });
+                  }
+                });
+              }
+            })
+          })
+        }
+      });
+    }
+  });
+});
+
+app.post('/api/users/logout', function(req, res) {
+  Token.findOne({'token': req.body.token}, function(err, token) {
+    if (err) {
+      res.status(400).json(err);
+    };
+    if (token === null) {
+      res.status(400).json({"Error": "Token not found."});
+    } else {
+      token.remove(function(err) {
+        if (err) {
+          res.status(400).json(err);
+        } else {
+          res.status(200).json({
+            "Success": true
+          });
+        }
+      })
+    }
+  })
+});
 
 console.log('Express started. Listening on port', process.env.PORT || 3000);
 app.listen(process.env.PORT || 3000);
