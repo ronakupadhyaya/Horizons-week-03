20c0af913d2537b0fb8e9d0138f7b2e7db6948cb
Moose Paksoy <mustafa@joinhorizons.com>
2017-06-13 00:32:07 -0700

    wip

--- day4/examples/ajax-counter/app.js
+++ day4/examples/ajax-counter/app.js
@@ -1,31 +1,40 @@
+"use strict";
+
 var express = require('express');
+var app = express();
 var path = require('path');
 
-var app = express();
+// Set up handlebar templates
+var exphbs = require('express-handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
-// Add static files to Express
+// Enable POST request body parsing
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Redirect / to /index.html since we're not going to be rendering
-// html in the server in this exercise
-app.get('/', function(req, res) {
-  res.redirect('/index.html');
-});
-
 // We use a variable to keep count
-var counter = 0;
+var count = 0;
 
-// This is a JSON endpoint that increases the count and returns its current
-// value.
-app.post('/increment', function(req, res) {
-  counter++;
-  res.json({
-    count: counter
+app.get('/', function(req, res) {
+  res.render('index.hbs', {
+    count: count
   });
 });
 
+// ---Task 1---
+// POST /up: Create JSON endpoint that increases `count` by 1
+// and returns a JSON response indicating the new value of `count`.
+
+// YOUR CODE HERE
+
 // ---Task 2---
-// POST /decrement: Create another JSON endpoint that decreases `count` by 1
+// POST /down: Create another JSON endpoint that decreases `count` by 1
 // and returns a JSON response indicating the new value of `count`.
 
 // YOUR CODE HERE
