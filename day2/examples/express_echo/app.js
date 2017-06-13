540f0c8051f2aa50f106fa679dd30fb1a6303421
Augustus Chang <augustus.chang@rutgers.edu>
2017-06-13 09:21:25 -0700

    query params

--- day2/examples/express_echo/app.js
+++ day2/examples/express_echo/app.js
@@ -6,15 +6,18 @@ var app = express();
 
 // Example route:
 // This creates an Express route at http://localhost:3000
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.send('Express is running!')
-});
+});*/
 
 // Create a route that listens to /hello and takes one query parameter
 // name and responds with 'Hello there NAME!'
 // You can access the query parameter 'name' via request.query.name.
 
 // YOUR CODE HERE
+app.get('/', function(request, response) {
+	response.send("Hello there " + request.query.name);
+});
 
 // Start the server listening on port 3000.
 app.listen(3000);
