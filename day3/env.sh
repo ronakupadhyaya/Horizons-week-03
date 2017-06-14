387d58f4a3199d866bc6f1fdfd45bc4db83bc39f
Amanda <ajhwhiz@gmail.com>
2017-06-14 11:16:15 -0700

    day 3 morning exercises cntd

--- day3/env.sh
+++ day3/env.sh
@@ -1,1 +1,1 @@
-export MONGODB_URI = "mongodb://accio:Apples123@ds161225.mlab.com:61225/amanda-horizons"
+export MONGODB_URI='mongodb://accio:Apples123@ds161225.mlab.com:61225/amanda-horizons'
--- day3/mongo.js
+++ day3/mongo.js
@@ -1,4 +1,27 @@
 "use strict";
-
+// console.log(process.env.MONGODB_URI);
 var mongoose = require('mongoose')
-mongoose.connect()
+mongoose.connect(process.env.MONGODB_URI)
+
+var Student = mongoose.model('Student', {
+  name: String
+});
+
+// create new student max
+var max = new Student({name: 'Max'})
+max.save(function(err) {
+  if (err) {
+    console.log('could not save', err);
+  } else {
+    console.log('success');
+  }
+})
+
+//find all students
+// Student.find(function(err, students) {
+//   if (err) {
+//     console.log('something went wrong', err);
+//   } else {
+//     console.log('students', students);
+//   }
+// })
