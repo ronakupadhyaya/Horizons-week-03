440b30778c8eb4270ea40662943c0c10c3d5c559
Quinntin Ruiz <[quinntin.ruiz@gmail.com]>
2017-06-14 13:15:08 -0700

    morning exc

--- day3/models/models.js
+++ day3/models/models.js
@@ -9,12 +9,34 @@ if (! process.env.MONGODB_URI) {
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
-var Cat; // YOUR CODE HERE - define the cat model
-
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
+var Cat = mongoose.model("Cat", {
+  name: {
+    type: String,
+    required: true;
   }
+})
+//you can make objects inside of models to make constraints, such as making it
+//mandatory for the user to enter a name for the cat 
+
+Cat.find({
+  name: 'Pradyut'
+}, function(err, cats) {
+  console.log('cats', cats);
 });
+
+
+var pradyut = new Cat({name:'Pradyut'});
+var sarah = new Cat({name:'Sarah'});
+
+pradyut.save(function(){
+  sarah.save(function(){
+    Cat.find(function(error, cats) {
+      if (error) {
+        console.log("Can't find cats", error);
+      } else {
+        console.log('Cats', cats);
+      }
+    });
+  })
+
+  });
