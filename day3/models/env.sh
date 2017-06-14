a933906143ac4d5b8e802fb18b05f9f94dd5e9f5
Benjamin <B@b.com>
2017-06-14 10:39:31 -0700

    in class excersise work

---
+++ day3/models/env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI=mongodb://Benjamin:12345678@ds153659.mlab.com:53659/benjamin-horizons
--- day3/models/models.js
+++ day3/models/models.js
@@ -1,13 +1,43 @@
 // First let's set up our MongoDb connection
 var mongoose = require('mongoose');
-mongoose.connect(require('./connect'));
+mongoose.connect(process.env.MONGODB_URI);
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+var Cat = mongoose.model('Cat', {
+  name: String,
+  furColor: String
+})
+var black = new Cat({
+  name: "Crookshanks",
+  furColor: "Black"
+});
 
-Cat.find(function(error, cats) {
+var white = new Cat({
+  name: "Mr. Bigglesworth",
+  furColor: "White"
+});
+var Calico = new Cat({
+  name: "Empurress",
+  furColor: "Calico"
+});
+
+// black.save();
+// white.save();
+// Calico.save();
+
+Cat.find(function (error, cats) {
   if (error) {
     console.log("Can't find cats", error);
   } else {
     console.log('Cats', cats);
+  }
+});
+
+Cat.findOne({
+  name: "Mr. Bigglesworth"
+}, function (error, cats) {
+  if (error) {
+    console.log("Can't find cats", error);
+  } else {
+    console.log('I found him ', cats);
   }
 });
