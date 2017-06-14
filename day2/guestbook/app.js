0ae36c65ec2c549ed4fa9a3f5c86e890cc143ee3
git status <phennessey19@cmc.edu>
2017-06-13 17:19:48 -0700

    w3d2

--- day2/guestbook/app.js
+++ day2/guestbook/app.js
@@ -44,7 +44,7 @@ app.get('/', function(req, res) {
 //
 // For example if you wanted to render 'views/index.hbs' you'd do res.render('index')
 app.get('/login', function(req, res) {
-  // YOUR CODE HERE
+  res.render('login')
 });
 
 // POST /login: Receives the form info from /login, sets a cookie on the client
@@ -64,11 +64,52 @@ app.post('/login', function(req, res) {
 // Hint: to get the username, use req.cookies.username
 // Hint: use data.read() to read the post data from data.json
 app.get('/posts', function (req, res) {
+  var order = req.query.order;
+  var newList = data.read();
+  var list = data.read();
+  if(order === 'ascending'){
+    newList.sort(function(a,b){
+      //console.log(new Date(a.date).getTime());
+      return new Date(a.date).getTime() - new Date(b.date).getTime();
+    })
+    res.render('posts',{
+      username: req.cookies.username,
+      posts: newList
+    })
+  }
+  else if(order === 'descending'){
+    newList.sort(function(a,b){
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    })
+    res.render('posts',{
+      username: req.cookies.username,
+      posts: newList
+    }); 
+  }
+  else {
+
+  var para = req.query.author;
+  console.log(para);
+  if(para) {
+  var listFiltered = newList.filter(function(item){
+    console.log(item.author);
+    if(item.author === para) return item;
+  })
+  // console.log(newList);
   res.render('posts', {
-    // Pass `username` to the template from req.cookies.username
-    // Pass `posts` to the template from data.read()
-    // YOUR CODE HERE
+    username: req.cookies.username, 
+    posts: listFiltered
+  })
+    
+  }
+else {
+    res.render('posts', {
+      username: req.cookies.username,
+      posts: list
   });
+  
+}
+  }
 });
 
 // ---Part 3. Create new posts---
@@ -81,8 +122,10 @@ app.get('/posts', function (req, res) {
 //
 // Hint: check req.cookies.username to see if user is logged in
 app.get('/posts/new', function(req, res) {
-  // YOUR CODE HERE
+  res.render('post_form')
 });
+
+
 
 // POST /posts:
 // This route is called by the form on /posts/new when a new post is being created.
@@ -101,7 +144,27 @@ app.get('/posts/new', function(req, res) {
 // Read all posts with data.read(), .push() the new post to the array and
 // write it back wih data.save(array).
 app.post('/posts', function(req, res) {
-  // YOUR CODE HERE
+  if (req.cookies.username === undefined) {
+    res.status(401).send("Please Log In!")
+  }
+  // console.log(req.cookies.username);
+  // console.log(req.body.title);
+  // console.log(req.body.body);
+  // console.log(req.body.date1);
+  else if (req.body.title === undefined || req.body.body === undefined || req.body.date1 === undefined) {
+    res.status(400).send("Please include all inputs!");
+  }
+  else {
+    var posts = data.read();
+    posts.push({
+        title: req.body.title,
+        body: req.body.body,
+        author: req.cookies.username,
+        date: req.body.date1,
+      });
+    data.save(posts);
+    res.redirect('/posts');
+  }
 });
 
 // Start the express server
