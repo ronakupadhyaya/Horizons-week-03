fd14c5483ac213c99131e4cca51e44248d2ba781
marinabacha <marinabacha@nyu.edu>
2017-06-14 13:15:46 -0700

    second commit from day 3 week "
    :q

--- day3/models/models.js
+++ day3/models/models.js
@@ -8,8 +8,40 @@ if (! process.env.MONGODB_URI) {
 // First let's set up our MongoDb connection
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
+//the line below creates a new model, the first string is the name of the collection, that string will become pluralized
+//the other are the characteristics
+var Cat = mongoose.model('Cat',{name:String,furColor:String})
 
-var Cat; // YOUR CODE HERE - define the cat model
+var cook= new Cat({name:'Crookshanks', furColor:"Black"})
+var biggles= new Cat({name:"Mr. Bigglesworth", furColor:"White"})
+var empurress= new Cat({name:"Empurress", furColor:"Calico"})
+
+cook.save(function(err){
+  if(err){
+    console.log('could not save', err)
+  }
+  else{
+    console.log('success')
+  }
+})
+
+biggles.save(function(err){
+  if(err){
+    console.log('could not save', err)
+  }
+  else{
+    console.log('success')
+  }
+})
+
+empurress.save(function(err){
+  if(err){
+    console.log('could not save', err)
+  }
+  else{
+    console.log('success')
+  }
+})
 
 Cat.find(function(error, cats) {
   if (error) {
@@ -18,3 +50,24 @@ Cat.find(function(error, cats) {
     console.log('Cats', cats);
   }
 });
+Cat.find({name: "Mr. Bigglesworth"},function(error, cats) {
+  if (error) {
+    console.log("Can't find cats", error);
+  } else {
+    console.log('Cats', cats);
+  }
+});
+
+//to fix asynchronous
+// empuress.save(function(){
+//   biggles.save(function(){
+//
+//     Cat.find(function(error,cats){
+//       if(error){
+//         console.log("can't find cats", error);
+//       }else{
+//         console.log('cats',cats);
+//       }
+//     })
+//   })
+// })
