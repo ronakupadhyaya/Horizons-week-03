4eec0c605e95520015c56cd3261106e5129a000d
Meber5 <meberle@exeter.edu>
2017-06-14 11:51:10 -0700

    changes

---
+++ day3/models/env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI="mongodb://test:test@ds153659.mlab.com:53659/meber5-horizons";
--- day3/models/models.js
+++ day3/models/models.js
@@ -1,13 +1,26 @@
 // First let's set up our MongoDb connection
 var mongoose = require('mongoose');
-mongoose.connect(require('./connect'));
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+if(!process.env.MONGODB_URI){
+  console.log("no");
+  exit(1);
+}
 
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
-});
+mongoose.connect(process.env.MONGODB_URI);
+
+var Dog = mongoose.model('Dog', {name: String, furColor: String})
+
+var Cooper = new Dog({name: 'Cooper', furColor: 'Black and White'});
+var Murphy = new Dog({name: 'Murphy', furColor: 'Brown and White'});
+
+//Cooper.save(function(){
+  //Murphy.save(function(){
+    Dog.find({name: 'Cooper'}, function(error, dogs) {
+      if (error) {
+        console.log("Can't find dogs", error);
+      } else {
+        console.log('dogs', dogs);
+      }
+    });
+  //});
+//});
