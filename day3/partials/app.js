b7c9b6b4a08668ec0fdb4959b79aaf4e3bdd6ceb
Amanda <ajhwhiz@gmail.com>
2017-06-14 13:10:19 -0700

    day 3 morning cntd

--- day3/partials/app.js
+++ day3/partials/app.js
@@ -4,7 +4,10 @@ var exphbs  = require('express-handlebars');
 
 var app = express();
 
-app.engine('hbs', exphbs({extname:'hbs'}));
+app.engine('hbs', exphbs({
+  extname:'hbs',
+  defaultLayout: 'main.hbs'
+}));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,6 +15,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('index.hbs');
+});
+
+app.get('/second', function (req, res) {
+    res.render('second.hbs');
 });
 
 
