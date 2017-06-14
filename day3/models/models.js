67049658463dd19c9a9259fe8cde3473e8c7dff2
Nate <n_straus3@outlook.com>
2017-06-14 11:28:18 -0700

    models

--- day3/models/models.js
+++ day3/models/models.js
@@ -1,6 +1,6 @@
 "use strict";
 
-if (! process.env.MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   console.error('MONGODB_URI missing, make sure you run "source env.sh"');
   process.exit(1);
 }
@@ -9,12 +9,43 @@ if (! process.env.MONGODB_URI) {
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
-var Cat; // YOUR CODE HERE - define the cat model
+var Cat = mongoose.model('cats', {
+  name: String,
+  furColor: String
+});
 
-Cat.find(function(error, cats) {
+var crookshanks = new Cat({ name: 'Crookshanks', furColor: 'Black' });
+var mrB = new Cat({ name: 'Mr. Bigglesworth', furColor: 'White' });
+var empurress = new Cat({ name: 'Empurress', furColor: 'Calico' });
+
+crookshanks.save(checkCats);
+mrB.save(checkCats);
+empurress.save(checkCats);
+
+function checkCats(err) {
+  saveHelper(err, this);
+  Cat.find(function (error, cats) {
+    if (error) {
+      console.log("Can't find cats", error);
+    } else {
+      console.log('Cats', cats);
+    }
+  });
+}
+
+Cat.findOne({ name: 'Mr. Bigglesworth' }, function (error, cats) {
   if (error) {
     console.log("Can't find cats", error);
   } else {
     console.log('Cats', cats);
   }
 });
+
+var saveHelper = function (err) {
+  if (err) {
+    console.log('Could not save', err);
+  }
+  else {
+    console.log('Saved');
+  }
+}
