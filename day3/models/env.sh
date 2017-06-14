01f311ad37841e4ac1370fbac9ea94ecf946d8b9
Todd <allento@sas.upenn.edu>
2017-06-14 12:37:19 -0400

    day3 stuff

---
+++ day3/models/env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI=mongodb://toddallen115:metalconcert1@ds153659.mlab.com:53659/toddhorizons
--- day3/models/models.js
+++ day3/models/models.js
@@ -1,13 +1,42 @@
 // First let's set up our MongoDb connection
 var mongoose = require('mongoose');
-mongoose.connect(require('./connect'));
+mongoose.connect(process.env.MONGODB_URI);
 
-var Cat = mongoose.model('Cat', {name: String, furColor: String})
+var Cat = mongoose.model('Cat', {name: String, furColor: String});
 
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
+// var
+
+
+
+var crookshanks = new Cat({name: 'Crookshanks', furColor: 'Black'});
+var big = new Cat({name: 'Mr. Bigglesworth', furColor: 'White'});
+var emp = new Cat({name: 'Empurress', furColor: 'Calico'});
+
+// crookshanks.save(function(err){
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log("success");
+//   }
+// });
+// big.save(function(err){
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log("success");
+//   }
+// });
+// emp.save(function(err){
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log("success");
+//   }
+// });
+
+//console.log(Cat.findOne({name: 'Mr. Bigglesworth'}));
+
+Cat.findOne({ 'name': 'Mr. Bigglesworth' }, 'name furColor', function (err, cat) {
+  if (err) console.log(err);
+  console.log('%s has %s fur.', cat.name, cat.furColor);
 });
