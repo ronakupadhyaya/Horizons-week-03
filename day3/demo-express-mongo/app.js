05a4aadb602cfb597ac5de9c6f7a1122afca2a59
Moose Paksoy <mustafa@joinhorizons.com>
2017-06-14 07:50:24 -0700

    Clean demo.

--- day3/demo-express-mongo/app.js
+++ day3/demo-express-mongo/app.js
@@ -1,48 +1,18 @@
+"use strict";
+
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
-// MONGOOSE
-var mongoose = require('mongoose');
-mongoose.connect(require('./connect'));
-var Cat = mongoose.model('Cat', {name: String, furColor: String});
-// MONGOOSE
-
 var app = express();
-
-// views/partials
-
 app.engine('hbs', exphbs({
-  'extname': 'hbs',
-  'defaultLayout': 'main'
+  'extname': 'hbs'
 }));
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
-  var mongoQuery = { };
-  if (req.query.name) {
-    mongoQuery.name = req.query.name;
-  }
-  Cat.find(mongoQuery, function(error, catsFromMongo) {
-    res.render('index.hbs', {
-      cats: catsFromMongo
-    });
-  })
+  res.render('index.hbs');
 });
 
-app.get('/other', function(req, res) {
-  res.render('other');
+app.listen(3000, function() {
+  console.log('Running on port 3000!');
 });
-
-
-// app.engine('hbs', exphbs({extname:'hbs', defaultLayout: 'main.hbs'}));
-// app.set('view engine', 'hbs');
-//
-// app.get('/', function (req, res) {
-//     res.render('index.hbs');
-// });
-//
-// app.get('/other', function (req, res) {
-//     res.render('other.hbs');
-// });
-
-app.listen(3000);
