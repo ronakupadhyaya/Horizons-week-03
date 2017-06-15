3befec83495a7b9aa1f908a1cee27f5f1459e391
Augustus Chang <augustus.chang@rutgers.edu>
2017-06-15 09:10:01 -0700

    day3

--- day3/horizonstarter/models.js
+++ day3/horizonstarter/models.js
@@ -1,4 +1,9 @@
 "use strict";
+// title: make this field required
+// goal: Type: Number, required
+// description: Type: String
+// start: Type: Date, required
+// end: Type: Date, required
 
 // Project model
 var mongoose = require('mongoose');
@@ -6,8 +11,27 @@ var mongoose = require('mongoose');
 var Project = mongoose.model('Project', {
   title: {
     type: String,
-  }
-  // YOUR CODE HERE
+    required: true
+  },
+  goal: {
+    type: Number,
+    required: true
+  },
+  description: String,
+  start: {
+    type: Date,
+    required: true
+  },
+  end: {
+    type: Date,
+    required: true
+  },
+  contributions: [{
+    name: String,
+    amount: Number
+  }],
+  totalAmount: Number,
+  percentage: Number,
 });
 
 module.exports = {
--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -23,31 +23,120 @@ router.get('/create-test-project', function(req, res) {
 // Part 1: View all projects
 // Implement the GET / endpoint.
 router.get('/', function(req, res) {
-  // YOUR CODE HERE
+  Project.find(function(err, project_arr){
+
+    if(err){
+      console.log("ERROR IN GET /");
+    }else{
+      res.render('index', {
+        project_arr: project_arr
+      });
+    }
+
+  });
 });
 
 // Part 2: Create project
 // Implement the GET /new endpoint
 router.get('/new', function(req, res) {
-  // YOUR CODE HERE
+  res.render('new');
 });
 
 // Part 2: Create project
 // Implement the POST /new endpoint
 router.post('/new', function(req, res) {
-  // YOUR CODE HERE
+
+  if(req.body.title && req.body.goal && req.body.start && req.body.end){
+
+    var my_post = new Project({
+      title: req.body.title,
+      goal: req.body.goal,
+      description: req.body.description,
+      start: req.body.start,
+      end: req.body.end
+    });
+
+    my_post.save(function(err){
+      if(err){
+        console.log("ERROR SAVING");
+      }else{
+        console.log("POST SUCCESSFUL");
+      }
+    });
+    res.redirect('/');
+  }else{
+    var title = (req.body.title) ? req.body.title : "Error: title required";
+    var goal = (req.body.goal) ? req.body.goal : "Error: goal required";
+    var start = (req.body.start) ? req.body.start : "Error: start required";
+    var end = (req.body.end) ? req.body.end : "Error: end required";
+
+    console.log("ERROR NOT COMPLETE");
+
+    res.render('new', {
+      title: title,
+      goal: goal,
+      description: req.body.description,
+      start: start,
+      end: end
+    });
+
+  }
 });
 
 // Part 3: View single project
 // Implement the GET /project/:projectid endpoint
 router.get('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  var id = req.params.projectid;
+  Project.findById(id, function(err, project){
+    if(err){
+      console.log("ERROR in GET /project/:projectid");
+      res.sendStatus(404);
+    }else{
+      if(project){
+        res.render('project', {
+          project: project
+        });
+      }else{
+        res.sendStatus(404);
+      }
+    }
+  })
 });
 
 // Part 4: Contribute to a project
-// Implement the GET /project/:projectid endpoint
+// Implement the POST /project/:projectid endpoint
 router.post('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  // get id
+  var id = req.params.projectid;
+
+  console.log("id is", id);
+
+  // find project document via id
+  Project.findById(id, function(err, project){
+
+    if(err){
+      console.log("ERROR in POST /post/:projectid");
+      res.sendStatus(404);
+    }else{
+      // if contributions exist
+      if(project.contributions.length > 0){
+        console.log("had previous contributions");
+        project.contributions.push({name: req.body.name, amount: req.body.amount});
+        project.totalAmount += parseInt(req.body.amount);
+      }else{
+        project.contributions = [{name: req.body.name, amount: req.body.amount}];
+        project.totalAmount = parseInt(req.body.amount);
+      }
+      // get percentage
+      project.percentage = Math.floor(project.totalAmount/project.goal * 100);
+
+      project.save();
+
+      res.redirect(`/project/${id}`);
+    }
+  });
+
+
 });
 
 // Part 6: Edit project
