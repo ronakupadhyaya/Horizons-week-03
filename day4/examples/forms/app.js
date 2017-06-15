99d80613d88a0ccb022e4b4ed50cfe42794a8315
Mason <lantay@alumni.unc.edu>
2017-06-15 13:16:28 -0700

    half of day 4

--- day4/examples/forms/app.js
+++ day4/examples/forms/app.js
@@ -1,4 +1,4 @@
-"use strict";
+ "use strict";
 
 var express = require('express');
 var app = express();
@@ -10,14 +10,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-// Enable form validation with express validator.
-var expressValidator = require('express-validator');
-app.use(expressValidator());
 
 // Enable POST request body parsing
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Enable form validation with express validator.
+var expressValidator = require('express-validator');
+app.use(expressValidator());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,6 +36,7 @@ app.get('/register', function(req, res){
   res.render('register');
 });
 
+
 // POST /register
 // This is the endpoint that the user hits when they submit
 // the registration form.
@@ -45,13 +47,26 @@ app.get('/register', function(req, res){
 //    profile should not be editable.
 app.post('/register', function(req, res){
   // YOUR CODE HERE - Add express-validator validation rules here
-  var errors; // YOUR CODE HERE - Get errors from express-validator here
+  console.log("hi");
+  req.check('firstName', 'error empty').notEmpty();
+  req.check('middleInitial', 'error wrong length').isLength({min:1, max:1});
+  req.check('lastName', 'error empty2').notEmpty();
+  req.check('dateOfBirth', 'error not date').isBefore(new Date().toString());
+  req.check('password', 'error empty3').notEmpty();
+  req.check('repeatPassword', 'error wrong password repeat').equals(req.body.password);//TODO add a second method to do same as above
+  req.check('gender', 'error empty4').notEmpty();
+
+  var errors=req.validationErrors(); // YOUR CODE HERE - Get errors from express-validator here
+  console.log(errors);
   if (errors) {
     res.render('register', {errors: errors});
   } else {
     // Include the data of the profile to be rendered with this template
     // YOUR CODE HERE
-    res.render('profile');
+    var data={
+      profile: req.body
+    }
+    res.render('profile', data);
   }
 });
 
