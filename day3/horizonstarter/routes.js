35054947daf8715a990b123a7338a1b5f35ae160
zioutang <tangziou@gmail.com>
2017-06-16 11:04:56 -0700

    w3d4

--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -5,7 +5,8 @@ var express = require('express');
 var router = express.Router();
 var Project = require('./models').Project;
 var strftime = require('strftime');
-
+var expressValidator = require('express-validator');
+router.use(expressValidator());
 // Example endpoint
 router.get('/create-test-project', function(req, res) {
   var project = new Project({
@@ -20,18 +21,101 @@ router.get('/create-test-project', function(req, res) {
   });
 });
 
-// Part 1: View all projects
-// Implement the GET / endpoint.
+// part 7:  Sort projects
 router.get('/', function(req, res) {
   // YOUR CODE HERE
-  Project.find(function(err, array) {
+  if (req.query.sort) {
+    if (req.query.sort === 'contributions') {
+      Project.find(function(err, array) {
+        array.sort(function(a, b) {
+          var contributionA = a.contributions;
+          var contributionB = b.contributions
+          var totalEachA = 0;
+          var totalEachB = 0;
+          contributionA.forEach(function(ele) {
+            totalEachA += parseInt(ele.amount);
+          })
+          contributionB.forEach(function(item) {
+            totalEachB += parseInt(item.amount);
+          })
+          if (totalEachA === undefined) {
+            totalEachA = 0;
+          }
+          if (totalEachB === undefined) {
+            totalEachB = 0;
+          }
+          return totalEachA - totalEachB;
+        })
+        res.render('index', {
+          items: array
+        });
+      });
 
-    res.render('index', {
-      items: array
-    });
+    } else {
+      var sortObject = {};
+      sortObject[req.query.sort] = 1;
+      Project.find().sort(sortObject).exec(function(err, array) {
+        res.render('index', {
+          items: array
+        });
+        // YOUR CODE HERE
+      });
+    }
+  } else {
+    if (req.query.view === 'full') {
+      Project.find(function(err, array) {
+        var newList = array.filter(function(item) {
+          var contribution = item.contributions;
+          var totalAmount = 0;
+          contribution.forEach(function(ele) {
+            totalAmount += parseInt(ele.amount);
 
-  });
+          })
+          return totalAmount >= parseInt(item.goal);
+        })
+        res.render('index', {
+          items: newList
+        });
+      });
+    } else if (req.query.view === 'notfull') {
+      Project.find(function(err, array) {
+        var newList = array.filter(function(item) {
+          var contribution = item.contributions;
+          var totalAmount = 0;
+          contribution.forEach(function(ele) {
+            totalAmount += parseInt(ele.amount);
+
+          })
+          return totalAmount < parseInt(item.goal);
+        })
+        res.render('index', {
+          items: newList
+        });
+      });
+    } else {
+      Project.find(function(err, array) {
+        res.render('index', {
+          items: array
+        });
+      });
+
+    }
+  }
+
 });
+
+// Part 1: View all projects
+// Implement the GET / endpoint.
+// router.get('/', function(req, res) {
+//   // YOUR CODE HERE
+//   Project.find(function(err, array) {
+//
+//     res.render('index', {
+//       items: array
+//     });
+//
+//   });
+// });
 
 // Part 2: Create project
 // Implement the GET /new endpoint
@@ -45,24 +129,41 @@ router.get('/new', function(req, res) {
 // Implement the POST /new endpoint
 router.post('/new', function(req, res) {
   // YOUR CODE HERE
-  // console.log(req.body.title);
-  // res.send('/');
-  var project = new Project({
-    title: req.body.title,
-    goal: req.body.goal,
-    description: req.body.description,
-    start: req.body.start,
-    end: req.body.end
-  });
+  req.checkBody('title', 'Title must not be empty').notEmpty();
+  req.checkBody('goal', 'Goal is required and must be an integer').notEmpty().isInt();
+  req.checkBody('start', 'Start date must not be empty').notEmpty();
+  req.checkBody('end', 'End date must not be empty').notEmpty();
+  var errors = req.validationErrors(); // YOUR CODE HERE - Get errors from express-validator here
+  if (errors) {
+    // console.log(req.body.category);
+    res.render('new', {
+      errors: errors,
+      title: req.body.title,
+      goal: req.body.goal,
+      description: req.body.description,
+      start: req.body.start,
+      end: req.body.end,
+      category: req.body.category // question why it doesn't change
+    });
+  } else {
+    var project = new Project({
+      title: req.body.title,
+      goal: req.body.goal,
+      description: req.body.description,
+      start: req.body.start,
+      end: req.body.end,
+      category: req.body.category
+    });
 
-  project.save(function(error) {
-    if (error) {
-      console.log(error);
-      throw new Error('there is an error');
-    } else {
-      res.redirect('/');
-    }
-  });
+    project.save(function(error) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.redirect('/');
+      }
+    });
+
+  }
 
 });
 
@@ -82,7 +183,7 @@ router.get('/project/:projectid', function(req, res) {
         total += parseInt(item.amount);
       })
       // console.log(sumAmount);
-
+      // console.log(found.category);
       res.render('project', {
         title: found.title,
         goal: found.goal,
@@ -91,7 +192,9 @@ router.get('/project/:projectid', function(req, res) {
         end: found.end,
         contributions: found.contributions,
         totalAmount: total,
-        percentage: Math.floor((total / found.goal) * 100)
+        percentage: Math.floor((total / found.goal) * 100),
+        category: found.category,
+        id: id
       })
     }
   })
@@ -107,7 +210,9 @@ router.post('/project/:projectid', function(req, res) {
       console.log(error);
       // throw new Error('there is an error');
     } else {
+
       // console.log(found);
+      // if ((req.body.name !== undefined) && (req.body.amount !== undefined)) {
       var newObject = {
         name: req.body.name,
         amount: req.body.amount
@@ -116,17 +221,77 @@ router.post('/project/:projectid', function(req, res) {
       found.save(function(error) {
         if (error) {
           console.log(error);
-          throw new Error('there is an error');
+          // throw new Error('there is an error');
         } else {
           res.redirect('/');
         }
       });
+      // }
     }
   })
 });
 
 // Part 6: Edit project
+router.get('/project/:projectid/edit', function(req, res) {
+  // console.log(123123);
+  // console.log(req.)
+  var id = req.params.projectid;
+  Project.findById(id, function(error, found) { // found is an object
+    var categories = ['Famous Muppet Frogs', 'Current Black Presidents',
+      'The Pen Is Mightier', 'Famous Mothers', 'Drummers Named Ringo',
+      '1-Letter Words', 'Months That Start With "Feb"', 'How Many Fingers Am I Holding Up',
+      'Potent Potables'
+    ];
+    categories = categories.map(function(i) {
+      return {
+        name: i,
+        selected: found.category === i ? true : false
+      }
+    });
+    if (error) {
+      console.log(error);
+      // throw new Error('there is an error');
+    } else {
+      res.render('editProject', {
+        title: found.title,
+        goal: found.goal,
+        description: found.description,
+        start: found.start.toISOString().slice(0, 10),
+        end: found.end.toISOString().slice(0, 10),
+        categories: categories,
+        id: id
+      })
+      // console.log(found);
+    }
+  });
+})
+
+router.post('/project/:projectid/edit', function(req, res) {
+
+  var id = req.params.projectid;
+  Project.findByIdAndUpdate(id, { /// quesiont how to remove the previous one
+    title: req.body.title,
+    goal: req.body.goal,
+    description: req.body.description,
+    start: req.body.start,
+    end: req.body.end,
+    // categories: categories
+    category: req.body.category
+    // YOUR CODE HERE
+  }, function(err, found) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+    // YOUR CODE HERE
+  });
+})
 // Create the GET /project/:projectid/edit endpoint
 // Create the POST /project/:projectid/edit endpoint
+
+
+
+
 
 module.exports = router;
--- day4/examples/forms/app.js
+++ day4/examples/forms/app.js
@@ -7,7 +7,9 @@ var path = require('path');
 // Set up handlebar templates
 var exphbs = require('express-handlebars');
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.engine('.hbs', exphbs({
+  extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 // Enable form validation with express validator.
@@ -16,14 +18,16 @@ app.use(expressValidator());
 
 // Enable POST request body parsing
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTES
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.redirect('/register');
 });
 
@@ -31,7 +35,7 @@ app.get('/', function(req, res){
 // This is the endpoint that the user loads to register.
 // It contains an HTML form that should be posted back to
 // the server.
-app.get('/register', function(req, res){
+app.get('/register', function(req, res) {
   res.render('register');
 });
 
@@ -43,15 +47,46 @@ app.get('/register', function(req, res){
 // 2. Pass in all the submitted user information (from req) when rendering profile.hbs
 // 3. Update profile.hbs to display all the submitted user profile fields. This
 //    profile should not be editable.
-app.post('/register', function(req, res){
+app.post('/register', function(req, res) {
   // YOUR CODE HERE - Add express-validator validation rules here
-  var errors; // YOUR CODE HERE - Get errors from express-validator here
+  req.checkBody('firstName', 'First Name Must not be empty').notEmpty();
+  req.checkBody('middleName', 'More than one letter').isLength({
+    max: 1
+  });
+  req.checkBody('lastName', 'last name must not be empty').notEmpty();
+  req.checkBody('dob', 'dob Must be a date in the past').isBefore();
+  req.checkBody('password', 'password  must not be empty').notEmpty();
+  req.checkBody('repeatPassword', 'repeatpasswoed must not be empty and matche each other').notEmpty().matches(req.body.repeatPassword);
+  req.checkBody('check', 'Must sign up').notEmpty();
+  var errors = req.validationErrors(); // YOUR CODE HERE - Get errors from express-validator here
   if (errors) {
-    res.render('register', {errors: errors});
+    res.render('register', {
+      errors: errors,
+      firstName: req.body.firstName,
+      middleName: req.body.middleName,
+      lastName: req.body.lastName,
+      dob: req.body.dob,
+      password: req.body.password,
+      repeat: req.body.repeatPassword,
+      options: req.body.option,
+      bio: req.body.bio,
+      check: req.body.check
+    });
   } else {
     // Include the data of the profile to be rendered with this template
     // YOUR CODE HERE
-    res.render('profile');
+    res.render('profile', {
+      firstName: req.body.firstName,
+      middleName: req.body.middleName,
+      lastName: req.body.lastName,
+      dob: req.body.dob,
+      password: req.body.password,
+      repeat: req.body.repeatPassword,
+      options: req.body.option,
+      bio: req.body.bio,
+      check: req.body.check
+
+    });
   }
 });
 
