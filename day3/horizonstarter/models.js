d9ce35999c719c7f6c4874de99da3466f8026c1e
Owen Hines <owenhines07@gmail.com>
2017-06-29 16:24:33 -0700

    Day 4 stuff (day 3 project additions)

--- day3/horizonstarter/models.js
+++ day3/horizonstarter/models.js
@@ -8,6 +8,13 @@ var Project = mongoose.model('Project', {
     type: String,
     required: true
   },
+  category: {
+    type: String,
+    enum: ['Famous Muppet Frogs', 'Current Black Presidents', 'The Pen Is Mightier',
+      'Famous Mothers', 'Drummers Named Ringo', '1-Letter Words', 'Months That Start With "Feb"',
+      'How Many Fingers Am I Holding Up', 'Potent Potables'
+    ]
+  },
   goal: {
     type: Number,
     required: true
--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -1,10 +1,21 @@
 "use strict";
 
+Date.prototype.yyyymmdd = function() {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
+
+  return [this.getFullYear(),
+    (mm > 9 ? '' : '0') + mm,
+    (dd > 9 ? '' : '0') + dd
+  ].join('-');
+};
+
 // Routes, with inline controllers for each route.
 var express = require('express');
 var router = express.Router();
 var Project = require('./models').Project;
 var strftime = require('strftime');
+var _ = require('underscore');
 
 // Example endpoint
 router.get('/create-test-project', function(req, res) {
@@ -23,12 +34,47 @@ router.get('/create-test-project', function(req, res) {
 // Part 1: View all projects
 // Implement the GET / endpoint.
 router.get('/', function(req, res) {
-  // YOUR CODE HERE
-  Project.find(function(err, arr) {
-    res.render('index', {
-      items: arr
+  var sort = req.query.sort || 'title';
+  var sortDirection = req.query.sortDirection || 'asc';
+  var descending = (sortDirection === 'desc' || sortDirection === 'descending' || sortDirection === '-1');
+  var sortObject = {};
+  sortObject[sort] = sortDirection;
+  if (sort === 'totalContributions') {
+    Project.find(function(err, arr) {
+      if (err) {
+        console.log(err);
+      } else {
+        arr.forEach(function(proj) {
+          sortArr.push(proj.toObject());
+        });
+        sortArr.forEach(function(proj) {
+          var total = 0;
+          proj.contributions.forEach(function(item) {
+            total += item.amount;
+          });
+          proj.totalContributions = total;
+        });
+        sortArr.sort(function(a, b) {
+          if (descending) {
+            return b.totalContributions - a.totalContributions;
+          } else {
+            return a.totalContributions - b.totalContributions;
+          }
+        });
+        res.render('index', {
+          items: sortArr,
+          descending: descending
+        })
+      }
     });
-  });
+  } else {
+    Project.find({}).sort(sortObject).exec(function(err, arr) {
+      res.render('index', {
+        items: arr,
+        descending: descending
+      });
+    });
+  }
 });
 
 // Part 2: Create project
@@ -44,6 +90,8 @@ router.post('/new', function(req, res) {
   req.checkBody('goal', 'Error: missing goal.').notEmpty();
   req.checkBody('start', 'Error: missing start date.').notEmpty();
   req.checkBody('end', 'Error: missing end date.').notEmpty();
+  req.checkBody('category', 'Error: missing category.').notEmpty();
+
   if (req.validationErrors()) {
     console.log(req.validationErrors());
     res.render('new', {
@@ -57,6 +105,7 @@ router.post('/new', function(req, res) {
   } else {
     var newProj = new Project({
       title: req.body.title,
+      category: req.body.category,
       goal: req.body.goal,
       description: req.body.description,
       start: req.body.start,
@@ -117,6 +166,63 @@ router.post('/project/:projectid', function(req, res) {
 
 // Part 6: Edit project
 // Create the GET /project/:projectid/edit endpoint
+
+router.get('/project/:projectid/edit', function(req, res) {
+  Project.findById(req.params.projectid, function(err, proj) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.render('editProject', {
+        title: proj.title,
+        goal: proj.goal,
+        description: proj.description,
+        start: proj.start.yyyymmdd(),
+        end: proj.end.yyyymmdd(),
+        helpers: {
+          selected: function(category) {
+            if (category === proj.category)
+              return 'selected';
+          }
+        }
+      });
+    }
+  });
+});
 // Create the POST /project/:projectid/edit endpoint
+
+router.post('/project/:projectid/edit', function(req, res) {
+  req.checkBody('title', 'Error: missing title.').notEmpty();
+  req.checkBody('goal', 'Error: missing goal.').notEmpty();
+  req.checkBody('start', 'Error: missing start date.').notEmpty();
+  req.checkBody('end', 'Error: missing end date.').notEmpty();
+  req.checkBody('category', 'Error: missing category.').notEmpty();
+
+  if (req.validationErrors()) {
+    console.log(req.validationErrors());
+    res.render('editProject', {
+      title: req.body.title,
+      goal: req.body.goal,
+      description: req.body.description,
+      start: req.body.start,
+      end: req.body.end,
+      error: req.validationErrors()
+    });
+  } else {
+    Project.findByIdAndUpdate(req.params.projectid, {
+      title: req.body.title,
+      category: req.body.category,
+      goal: req.body.goal,
+      description: req.body.description,
+      start: req.body.start,
+      end: req.body.end
+    }, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect('/project/' + req.params.projectid);
+      }
+    });
+  }
+});
 
 module.exports = router;
