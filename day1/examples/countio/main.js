9f313ef0af8309e519ed1fe072e4abbb3cea7eb3
jeff <tangj1122@gmail.com>
2017-09-18 11:11:37 -0700

    finish sec 4: file i/o

--- day1/examples/countio/main.js
+++ day1/examples/countio/main.js
@@ -1,9 +1,15 @@
 // Modify anything you like. Remember the content of the program is not important.
 // This is just an example to help you practice reading and writing from and to files.
 
+var fs = require('fs');
+var fileStr = fs.readFileSync('./log.txt', 'utf8') + "\n";
+
 if(process.argv[2] === '-s' || process.argv[2] === '--stats' ) {
-    console.log("stats requested");
+  var fileSplit = fileStr.split("\n");
+  var fileArr = fileSplit.slice(1, fileSplit.length - 1)
+  console.log("The first run occured at: " + fileArr[0]);
+  console.log("The last run occured at: " + fileArr[fileArr.length-1]);
+  console.log("This process has been run a total of " + fileArr.length + " times.");
 } else {
-    console.log("ran at:" + new Date());
+  fs.writeFileSync( './log.txt', fileStr + new Date() );
 }
-    
