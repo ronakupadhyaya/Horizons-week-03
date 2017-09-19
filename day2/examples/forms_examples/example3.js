8ee2898be51832e080be23039a298fe3b1da3612
masonliyu <masonliyu@gmail.com>
2017-09-19 13:40:22 -0700

    Finish video exercises week03/day2

--- day2/examples/forms_examples/example3.js
+++ day2/examples/forms_examples/example3.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -9,11 +10,42 @@ app.engine('hbs', exphbs({extname:'hbs'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser({extended: true}));
+
+// parsing accounts.json
+var fs = require('fs');
+var accountsArray = JSON.parse(fs.readFileSync('./accounts.json', 'utf8'));
+
 
 app.get('/', function(req, res) {
   res.render('example3');
 });
 
+app.post('/login', function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var validLogin = false;
+  var user;
+  for(var i = 0; i < accountsArray.length; i++) {
+    if(accountsArray[i].email === email) {
+      if(accountsArray[i].password === password) {
+        validLogin = true;
+        user = accountsArray[i];
+      }
+      break;
+    }
+  }
+  if(validLogin) { //correct user and password
+    res.render('example3', {
+      valid: true,
+      firstName: user.first_name
+    });
+  } else { //incorrect user or password
+    res.render('example3', {
+      invalid: true
+    });
+  }
+});
 // start the express app
 var port = process.env.PORT || 3000;
 app.listen(port);
