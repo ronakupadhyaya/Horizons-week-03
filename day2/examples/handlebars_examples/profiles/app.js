bf6c57d60ad90515dbb88562688fa352367207f7
mschw90 <Mschw90@gmail.com>
2017-09-19 14:51:43 -0700

    day 2 week 3

--- day2/examples/handlebars_examples/profiles/app.js
+++ day2/examples/handlebars_examples/profiles/app.js
@@ -9,8 +9,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // YOUR CODE HERE
+app.get('/', function(req, res) {
+  res.render('index', {
+    matt: data
+  })
+})
+
+var male = [];
+var female = [];
+
+for(var i = 0; i < data.length; i++){
+  if(data[i]["gender"] === "Male"){
+    male.push(data[i])
+  } else {
+    female.push(data[i])
+  }
+}
+
+app.get('/male', function(req, res) {
+  res.render('index', {
+    matt: male
+  })
+})
+
+app.get('/female', function(req, res) {
+  res.render('index', {
+    matt: female
+  })
+})
+
 
 app.listen(3000);
-
-
-
