96f77e4c5e847306ffb5d160718f5580ac5ebe71
obadiar <ob.diar@gmail.com>
2017-09-20 11:48:55 -0700

    Finish day2

--- day3/demo/app.js
+++ day3/demo/app.js
@@ -1,18 +1,18 @@
 "use strict";
 
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 var app = express();
 app.engine('hbs', exphbs({
-  'extname': 'hbs'
+    'extname': 'hbs',
 }));
 app.set('view engine', 'hbs');
 
-app.get('/', function(req, res) {
-  res.render('index.hbs');
+app.get('/', function (req, res) {
+    res.render('index.hbs');
 });
 
-app.listen(3000, function() {
-  console.log('Running on port 3000!');
+app.listen(3000, function () {
+    console.log('Running on port 3000!');
 });
--- day3/models/models.js
+++ day3/models/models.js
@@ -1,6 +1,6 @@
 "use strict";
 
-if (! process.env.MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   console.error('MONGODB_URI missing, make sure you run "source env.sh"');
   process.exit(1);
 }
@@ -9,9 +9,42 @@ if (! process.env.MONGODB_URI) {
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
-var Cat; // YOUR CODE HERE - define the cat model
+var Cat = mongoose.model('Cat', {
+  name: String,
+  furColor: String
+})
 
-Cat.find(function(error, cats) {
+var cat1 = new Cat({
+  name: "Crookshanks",
+  furColor: "Black"
+});
+
+var cat2 = new Cat({
+  name: "Mr. Bigglesworth",
+  furColor: "White"
+});
+
+var cat3 = new Cat({
+  name: "Empurress",
+  furColor: "Calico"
+});
+
+var catArray = []
+catArray.push(cat1);
+catArray.push(cat2);
+catArray.push(cat3);
+
+for (var i = 0; i < catArray.length; i++) {
+  var element = catArray[i];
+  element.save(function (error) {
+    if (error)
+      console.log('Failed to save,', error)
+    else
+      console.log('Saved successfully!')
+  })
+}
+
+Cat.find({ name: 'Empuress' }, function (error, cats) {
   if (error) {
     console.log("Can't find cats", error);
   } else {
