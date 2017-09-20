ca428773bf5a3adf430feadea85f72c69304d9eb
borajimin <borajimin@gmail.com>
2017-09-20 11:51:41 -0700

    message

--- day3/models/models.js
+++ day3/models/models.js
@@ -9,12 +9,42 @@ if (! process.env.MONGODB_URI) {
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
-var Cat; // YOUR CODE HERE - define the cat model
-
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
+var Cat = mongoose.model('Cat',{
+  name: String,
+  furColor: String
 });
+
+var Biggle = new Cat({
+  name: 'Mr.Biggleworth',
+  furColor: 'White'
+}); // YOUR CODE HERE - define the cat model
+
+var Crookshanks = new Cat({
+  name: 'Crookshanks',
+  furColor: 'Black'
+});
+
+
+var Empress = new Cat({
+  name: 'Empurress',
+  furColor: 'Calico'
+});
+var oreo = new Cat({
+  name: 'Oreo',
+  furColor: 'B&W'
+});
+
+
+oreo.save(function(err){
+  if(err){
+    console.log("Couldn't save Crookshanks");
+  }else{
+    Cat.find(function(error, cats) {
+      if (error) {
+        console.log("Can't find cats", error);
+      } else {
+        console.log('Cats', cats);
+      }
+    });
+  }
+})
