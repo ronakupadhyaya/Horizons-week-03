71e1ba980cd5ac84e7c3502070bb0b3911f9b34a
Andros <wong22@hotmail.co.uk>
2017-09-20 11:15:34 -0700

    Finish all inline exercises

--- day3/models/models.js
+++ day3/models/models.js
@@ -9,12 +9,51 @@ if (! process.env.MONGODB_URI) {
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
-var Cat; // YOUR CODE HERE - define the cat model
-
-Cat.find(function(error, cats) {
-  if (error) {
-    console.log("Can't find cats", error);
-  } else {
-    console.log('Cats', cats);
-  }
+var Cat = mongoose.model('Cat', {name: String, furColor: String})
+ // YOUR CODE HERE - define the cat model
+var cat1 = new Cat({
+  name: "Crookshanks",
+   furColor: "Black"
 });
+
+var cat2 = new Cat({
+  name: "Mr. Bigglesworth",
+   furColor: "White"
+})
+
+var cat3 = new Cat({
+  name: "Empurress",
+   furColor: "Calico"
+})
+
+//callback HELL?!
+
+// cat1.save(function(err){
+//   if(err){
+//     console.log("FAILED", err);
+//   } else{
+//     cat2.save(function(err){
+//       if(err){
+//         console.log("failed", err);
+//       } else{
+//         cat3.save(function(err){
+//           if(err){
+//             console.log("failed", err);
+//           } else{
+//             Cat.find(function(error, cats) {
+//               if (error) {
+//                 console.log("Can't find cats", error);
+//               } else {
+//                 console.log('Cats', cats);
+//               }
+//             });
+//           }
+//         })
+//       }
+//     })
+//   }
+// })
+
+Cat.findOne({name: "Mr. Bigglesworth"}, function(error, cat){
+  console.log("FOUND ", cat );
+})
