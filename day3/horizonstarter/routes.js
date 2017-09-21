38cf0f8e5d92e270533a01b88179bb3f611f9793
masonliyu <masonliyu@gmail.com>
2017-09-20 18:55:49 -0700

    Finished up to part 4 of horizonstarter

--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -23,31 +23,144 @@ router.get('/create-test-project', function(req, res) {
 // Part 1: View all projects
 // Implement the GET / endpoint.
 router.get('/', function(req, res) {
-  // YOUR CODE HERE
+  Project.find(function(err, projects) {
+    if(err) {
+      res.status(400).show('Unable to find projects due to ' + err);
+    } else {
+      res.render('index', {
+        projectList: projects
+      });
+    }
+  });
 });
 
 // Part 2: Create project
 // Implement the GET /new endpoint
 router.get('/new', function(req, res) {
-  // YOUR CODE HERE
+  res.render('new')
 });
 
 // Part 2: Create project
 // Implement the POST /new endpoint
 router.post('/new', function(req, res) {
-  // YOUR CODE HERE
+  var title = req.body.title;
+  var goal = req.body.goal;
+  var description = req.body.description;
+  var start = req.body.start;
+  var end = req.body.end;
+  if(!( title && goal && start && end)) {
+    res.render('new', {
+      project: {
+        title,
+        goal,
+        description,
+        start,
+        end,
+      },
+      error: true
+    });
+  } else {
+    var newProject = new Project({
+      title,
+      goal,
+      description,
+      start,
+      end,
+    });
+    newProject.save(function(err) {
+      if(err) {
+        res.render('new', {
+          project: {
+            title,
+            goal,
+            description,
+            start,
+            end,
+          },
+          saveError: err
+        });
+        console.log(err);
+      } else {
+        res.redirect('/');
+      }
+    })
+  }
 });
 
 // Part 3: View single project
 // Implement the GET /project/:projectid endpoint
 router.get('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  var id = req.params.projectid;
+  Project.findById(id, function(err, project) {
+    if(err) {
+      res.status(400).show('Unable to get project with id ' + id);
+    } else {
+      var sum;
+      var goalPercent;
+      if(project.contributions) {
+        sum = 0;
+        project.contributions.forEach(function(con) {
+          sum += con.amount;
+        });
+        goalPercent = sum/project.goal;
+      }
+      res.render('project', {
+        project: project,
+        sum: sum,
+        goalPercent: goalPercent,
+      });
+    }
+  })
 });
 
 // Part 4: Contribute to a project
 // Implement the GET /project/:projectid endpoint
 router.post('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  var name = req.body.name;
+  var amount = req.body.amount;
+  var id = req.params.projectid;
+  Project.findById(id, function(err, project) {
+    if(err) {
+      res.status(400).show('Unable to access project with id ' + id);
+    } else {
+      if(isNaN(parseInt(amount))) { //user didn't type in a number
+        res.render('project', {
+          project: project,
+          numError: true,
+          name: name,
+          amount: amount,
+        });
+      } else { //it is a number
+        amount = parseInt(amount);
+        var newContribution = {
+          name,
+          amount,
+        };
+        if(!project.contributions) { //contributions doesn't exist yet
+          project.contributions = [newContribution];
+        } else { //it does exist so push to the end
+          project.contributions.push(newContribution);
+        }
+        // all work is done, save the new updated project
+        project.save(function(err) {
+          if(err) {
+            res.show('Failed to save updated project for reason: ', err);
+          } else {
+            var sum = 0;
+            project.contributions.forEach(function(con) {
+              sum += con.amount;
+            });
+            var goalPercent = (sum/project.goal)*100;
+            res.render('project', {
+              project: project,
+              sum: sum,
+              goalPercent: goalPercent,
+            })
+          }
+        });
+      }
+    }
+  });
 });
 
 // Part 6: Edit project
