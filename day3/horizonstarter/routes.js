ed9d5046ebb6d0bb982c0664141a9ec0de206ce7
Brock <bws219@lehigh.edu>
2017-09-20 21:16:56 -0700

    Day 3 Donezo

--- day3/horizonstarter/routes.js
+++ day3/horizonstarter/routes.js
@@ -5,6 +5,11 @@ var express = require('express');
 var router = express.Router();
 var Project = require('./models').Project;
 var strftime = require('strftime');
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+router.use(bodyParser.json());
 
 // Example endpoint
 router.get('/create-test-project', function(req, res) {
@@ -23,31 +28,111 @@ router.get('/create-test-project', function(req, res) {
 // Part 1: View all projects
 // Implement the GET / endpoint.
 router.get('/', function(req, res) {
-  // YOUR CODE HERE
+  Project.find(function(error, projects) {
+    if (error) {
+      res.send(`There was an error loading projects: ${error}`);
+    } else {
+      res.render('index', {
+        projects: projects
+      })
+    }
+  })
 });
 
 // Part 2: Create project
 // Implement the GET /new endpoint
 router.get('/new', function(req, res) {
-  // YOUR CODE HERE
+  res.render('new');
 });
 
 // Part 2: Create project
 // Implement the POST /new endpoint
 router.post('/new', function(req, res) {
-  // YOUR CODE HERE
+  var title = req.body.title;
+  var goal = req.body.goal;
+  var desc = req.body.description;
+  var start = req.body.start;
+  var end = req.body.end;
+  if (title && goal && start && end) {
+    var project = new Project({
+      title: title,
+      goal: goal,
+      description: desc,
+      start: start,
+      end: end,
+      contributions: []
+    })
+    project.save(function(err) {
+      if (err) {
+        res.render('new', {
+          error: "Error: there was a problem entering the data",
+          title: title || "Title",
+          goal: goal || 0,
+          desc: desc,
+          start: start,
+          end: end
+        })
+      } else {
+        res.redirect('/');
+      }
+    })
+  } else {
+    res.render('new', {
+      error: "you entered in some fucked up shit fam",
+      title: title || "Title",
+      goal: goal || 0,
+      desc: desc,
+      start: start,
+      end: end
+    })
+  }
 });
 
 // Part 3: View single project
 // Implement the GET /project/:projectid endpoint
 router.get('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  Project.findById(req.params.projectid, function(error, project) {
+    if (project) {
+      var total = project.contributions.reduce(function(a, b) {
+        return a + parseInt(b.amount);
+      }, 0);
+      res.render('project', {
+        id: req.params.projectid,
+        title: project.title,
+        goal: project.goal,
+        desc: project.description,
+        start: project.start.toDateString(),
+        end: project.end.toDateString(),
+        totalcont: total,
+        percentage: (total / project.goal) * 100,
+        contributions: project.contributions
+      })
+    } else {
+      res.send('This project does not exist');
+    }
+  })
 });
 
 // Part 4: Contribute to a project
 // Implement the GET /project/:projectid endpoint
 router.post('/project/:projectid', function(req, res) {
-  // YOUR CODE HERE
+  Project.findById(req.params.projectid, function(error, project) {
+    if (project) {
+      project.contributions.push({
+        name: req.body.name,
+        amount: req.body.amount
+      })
+      project.save(function(err) {
+        if (err) {
+          res.send("Some crazty bullshit went down")
+        } else {
+          res.redirect(`/project/${req.params.projectid}`);
+        }
+      })
+    } else {
+      res.send("crazy bullshit");
+    }
+  })
 });
 
 // Part 6: Edit project
--- day3/nba-mongoose/index.js
+++ day3/nba-mongoose/index.js
@@ -1,17 +1,27 @@
 // Require express and create an express app (Part 2.1)
+var express = require('express');
+var app = express();
 
 // Require mongoose (Part 2.2)
+var mongoose = require('mongoose');
 
 // Require and setup body-parser (Part 4.1)
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 // Require the Player model (Part 2.3)
+var Player = require('./model/player');
 
 // Require the Roster model (Part 5.2)
+var Roster = require('./model/roster');
 
 
 
 // Ensure that there is a MONGODB_URI environment variable (source env.sh)
-if (! process.env.MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
 }
 
@@ -24,6 +34,7 @@ mongoose.connection.on('error', function(err) {
   process.exit(1);
 });
 // Establish mongoose connection to the mongoDB on mlab (Part 2.2)
+mongoose.connect(process.env.MONGODB_URI);
 
 
 /* =====================================
@@ -31,13 +42,98 @@ mongoose.connection.on('error', function(err) {
    ===================================== */
 
 // (Part 3.1)
+app.get('/', function(req, res) {
+  Player.find(function(error, players) {
+    if (error) {
+      console.log("Error", error);
+    } else {
+      res.send(players);
+    }
+  })
+})
 
 // (Part 4.2)
+app.post('/addPlayer', function(req, res) {
+  var kwame = new Player({
+    Name: req.body.name,
+    Points: req.body.pts,
+    Rebounds: req.body.rbs,
+    Assists: req.body.ats
+  });
+  kwame.save(function(err) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(`Success ${kwame.Name} added!`)
+    }
+  })
+})
 
 // (Part 5.3)
+app.post('/addPlayerRoster', function(req, res) {
+  var roster = new Roster({
+    Name: req.body.name,
+    JerseyNumber: req.body.num,
+    Team: req.body.team
+  });
+  roster.save(function(err) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(`Success ${roster.Name} added to ${roster.Team}!`)
+    }
+  })
+})
+
+app.get('/:rosterid', function(req, res) {
+  Roster.findById(req.params.rosterid, function(error, roster) {
+    if (roster) {
+      var playertemp = {
+        Name: roster.Name,
+        Team: roster.Team,
+        JerseyNumber: roster.JerseyNumber
+      }
+      Player.findOne({
+        "Name": playertemp.Name
+      }, function(error, player) {
+        if (error) {
+          res.send(`There was an error finding ${playertemp.Name}.  He may not exist in the player database.`);
+        } else {
+          playertemp.Points = player.Points;
+          playertemp.Assists = player.Assists;
+          playertemp.Rebounds = player.Rebounds;
+          res.send(playertemp)
+        }
+      })
+    } else {
+      res.send(`The roster ID ${req.params.rosterid} does not exist`);
+    }
+  })
+})
 
 // (BONUS)
-
+app.delete('/:rosterid', function(req, res) {
+  Roster.findByIdAndRemove(req.params.rosterid, function(error, roster) {
+    if (roster) {
+      var name = roster.Name
+      Player.findOneAndRemove({
+          Name: name
+        },
+        function(error, player) {
+          if (error) {
+            res.send(`There was an error finding ${name}.  He may not exist in the player database.`);
+          } else {
+            res.send(`${name} was properly removed`)
+          }
+        })
+    } else {
+      res.send(`The roster ID ${req.params.rosterid} does not exist`);
+    }
+  })
+})
 
 
 // Begin listening on port 3000 (Part 2.1)
+app.listen(3000, function() {
+  console.log("Listenting on port 3000");
+})
