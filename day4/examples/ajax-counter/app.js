e81b5dddd22c5969552e1a9199c82bf8949e5715
jeff <tangj1122@gmail.com>
2017-09-21 11:47:17 -0700

    fin ajaxCounter

--- day4/examples/ajax-counter/app.js
+++ day4/examples/ajax-counter/app.js
@@ -22,22 +22,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 var count = 0;
 
 app.get('/', function(req, res) {
-  res.render('index.hbs', {
-    count: count
-  });
+  res.render('index.hbs', { count: count });
 });
 
 // ---Task 1---
 // POST /up: Create JSON endpoint that increases `count` by 1
 // and returns a JSON response indicating the new value of `count`.
-
 // YOUR CODE HERE
+app.post('/up', function(req, res) {
+  count++;
+  res.json({ count: count });
+});
 
 // ---Task 2---
 // POST /down: Create another JSON endpoint that decreases `count` by 1
 // and returns a JSON response indicating the new value of `count`.
-
 // YOUR CODE HERE
+app.post('/down', function(req, res) {
+  count--;
+  res.json({ count: count });
+});
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
